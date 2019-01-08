5dd0db653ef932e1eea3ee40f8f6c331bf52bdb5
pcwa-ahendricks <ahendricks@pcwa.net>
2019-01-07 18:23:57 -0800

    Forecast is deployed to now with monorepo.

--- packages/next/pages/index.js
+++ packages/next/pages/index.js
@@ -15,12 +15,12 @@ const index = () => {
             '//cosmic-s3.imgix.net/45960170-6294-11e7-8d46-7b261262b385-French_Meadows_Inlet_02.jpg'
         }}
       />
-      <ImgixFancy
+      {/* <ImgixFancy
         paddingPercent={'66.6495%'}
         src={
           '//cosmic-s3.imgix.net/45960170-6294-11e7-8d46-7b261262b385-French_Meadows_Inlet_02.jpg'
         }
-      />
+      /> */}
       <title>Welcome</title>
       <div style={{backgroundColor: 'beige', height: 1500}}>
         the next website.
@@ -32,10 +32,10 @@ const index = () => {
           position: 'relative'
         }}
       >
-        {/* <ImgixFancy
+        <ImgixFancy
           paddingPercent="129.3737%"
           src="//cosmic-s3.imgix.net/61bcf350-104d-11e9-81dd-490e145a6cb6-2018-YEAR-END-REPORT---FINAL.pdf"
-        /> */}
+        />
       </div>
       {/* <div style={{backgroundColor: 'beige', height: 1500}} /> */}
 
