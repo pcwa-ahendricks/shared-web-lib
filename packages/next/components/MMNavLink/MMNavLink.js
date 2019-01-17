f745d0550b040fcea8a225ac63d623080b22f1c0
pcwa-ahendricks <ahendricks@pcwa.net>
2019-01-17 14:28:32 -0800

    Use passHref prop.

--- packages/next/components/MMNavLink/MMNavLink.js
+++ packages/next/components/MMNavLink/MMNavLink.js
@@ -3,7 +3,6 @@ import React, {type Node} from 'react'
 import {withStyles} from '@material-ui/core/styles'
 import {Link, Typography as Type} from '@material-ui/core'
 import NextLink from 'next/link'
-import classNames from 'classnames'
 
 type Props = {
   classes: any,
@@ -14,17 +13,14 @@ type Props = {
 const styles = (theme) => ({
   text: {
     color: theme.palette.grey[50],
-    opacity: '0.85',
-    '&.navlink': {
-      cursor: 'pointer'
-    }
+    opacity: '0.85'
   }
 })
 
 const MMNavLink = ({classes, children, href}: Props) => {
   return (
-    <Type className={classNames(classes.text, 'navlink')}>
-      <NextLink href={href}>
+    <Type className={classes.text}>
+      <NextLink href={href} passHref>
         <Link color="inherit" underline="none">
           {children}
         </Link>
