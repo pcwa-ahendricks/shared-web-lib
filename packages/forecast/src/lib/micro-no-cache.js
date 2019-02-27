8d2d921065593a32639ffdf01d85afab4b2b9ee4
pcwa-ahendricks <ahendricks@pcwa.net>
2019-02-27 06:37:49 -0800

    Renamed returned function.

--- packages/forecast/src/lib/micro-no-cache.js
+++ packages/forecast/src/lib/micro-no-cache.js
@@ -2,16 +2,19 @@
 import {type ServerResponse, type IncomingMessage} from 'http'
 
 // Don't allow clients to cache responses by setting no cache headers.
-const noCache = (fn: Function) =>
-  function process(req: IncomingMessage, res: ServerResponse, ...args: any) {
-    res.setHeader('Surrogate-Control', 'no-store')
-    res.setHeader(
-      'Cache-Control',
-      'no-store, no-cache, must-revalidate, proxy-revalidate'
-    )
-    res.setHeader('Pragma', 'no-cache')
-    res.setHeader('Expires', '0')
-    return fn.apply(null, [req, res, args])
-  }
+const noCache = (next: Function) => (
+  req: IncomingMessage,
+  res: ServerResponse,
+  ...args: any
+) => {
+  res.setHeader('Surrogate-Control', 'no-store')
+  res.setHeader(
+    'Cache-Control',
+    'no-store, no-cache, must-revalidate, proxy-revalidate'
+  )
+  res.setHeader('Pragma', 'no-cache')
+  res.setHeader('Expires', '0')
+  return next.apply(null, [req, res, args])
+}
 
 export default noCache
