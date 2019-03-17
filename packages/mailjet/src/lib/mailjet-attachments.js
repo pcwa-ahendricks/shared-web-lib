cab9f2ea3a800583f9e313652a22cda4dd6c4c5d
pcwa-ahendricks <ahendricks@pcwa.net>
2019-03-17 10:27:49 -0700

    Remove confusing type.

--- packages/mailjet/src/lib/mailjet-attachments.js
+++ packages/mailjet/src/lib/mailjet-attachments.js
@@ -5,37 +5,35 @@ import {getType} from 'mime'
 import {type MailJetAttachment} from './types'
 
 const attach = (reqAttachments: Array<string>) => {
-  const attachments = reqAttachments.map<Promise<MailJetAttachment>>(
-    async (localFilePath: string) => {
-      try {
-        // We are supplying full path in attachments. So this isn't needed.
-        // const localFilePath = join(UPLOADS_DIR, ATTACHMENT_DIR, filename)
-        if (!(await stat(localFilePath))) {
-          throw new Error(
-            `Can't find specified file attachment at "${localFilePath}".`
-          )
-        }
-
-        const mimeType = getType(localFilePath)
-
-        //read the file
-        const buffer = await readFile(localFilePath)
-
-        //encode contents into base64
-        const b64 = buffer.toString('base64')
-        const basePath = parse(localFilePath).base
-
-        const attachment: MailJetAttachment = {
-          Filename: basePath,
-          ContentType: mimeType,
-          Base64Content: b64
-        }
-        return attachment
-      } catch (error) {
-        console.log(error)
+  const attachments = reqAttachments.map(async (localFilePath: string) => {
+    try {
+      // We are supplying full path in attachments. So this isn't needed.
+      // const localFilePath = join(UPLOADS_DIR, ATTACHMENT_DIR, filename)
+      if (!(await stat(localFilePath))) {
+        throw new Error(
+          `Can't find specified file attachment at "${localFilePath}".`
+        )
       }
+
+      const mimeType = getType(localFilePath)
+
+      //read the file
+      const buffer = await readFile(localFilePath)
+
+      //encode contents into base64
+      const b64 = buffer.toString('base64')
+      const basePath = parse(localFilePath).base
+
+      const attachment: MailJetAttachment = {
+        Filename: basePath,
+        ContentType: mimeType,
+        Base64Content: b64
+      }
+      return attachment
+    } catch (error) {
+      console.log(error)
     }
-  )
+  })
 
   return Promise.all(attachments)
 }
