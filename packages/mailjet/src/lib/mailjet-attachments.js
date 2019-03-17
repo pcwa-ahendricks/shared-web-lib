2b65ece02358b64f2d2f1016f6b4540d3ddeb7cb
pcwa-ahendricks <ahendricks@pcwa.net>
2019-03-17 10:27:03 -0700

    Use promises with mailjet-attachments.

--- packages/mailjet/src/lib/mailjet-attachments.js
+++ packages/mailjet/src/lib/mailjet-attachments.js
@@ -1,40 +1,43 @@
 // @flow
 import {parse} from 'path'
-import {existsSync, readFileSync} from 'fs'
+import {stat, readFile} from 'then-fs'
 import {getType} from 'mime'
 import {type MailJetAttachment} from './types'
 
 const attach = (reqAttachments: Array<string>) => {
-  const attachments = reqAttachments.map<?MailJetAttachment>(
-    (localFilePath: string) => {
-      // We are supplying full path in attachments. So this isn't needed.
-      // const localFilePath = join(UPLOADS_DIR, ATTACHMENT_DIR, filename)
-      if (!existsSync(localFilePath)) {
-        console.log(
-          `Can't find specified file attachment at "${localFilePath}".`
-        )
-        return null
+  const attachments = reqAttachments.map<Promise<MailJetAttachment>>(
+    async (localFilePath: string) => {
+      try {
+        // We are supplying full path in attachments. So this isn't needed.
+        // const localFilePath = join(UPLOADS_DIR, ATTACHMENT_DIR, filename)
+        if (!(await stat(localFilePath))) {
+          throw new Error(
+            `Can't find specified file attachment at "${localFilePath}".`
+          )
+        }
+
+        const mimeType = getType(localFilePath)
+
+        //read the file
+        const buffer = await readFile(localFilePath)
+
+        //encode contents into base64
+        const b64 = buffer.toString('base64')
+        const basePath = parse(localFilePath).base
+
+        const attachment: MailJetAttachment = {
+          Filename: basePath,
+          ContentType: mimeType,
+          Base64Content: b64
+        }
+        return attachment
+      } catch (error) {
+        console.log(error)
       }
-
-      const mimeType = getType(localFilePath)
-
-      //read the file
-      const buffer = readFileSync(localFilePath)
-
-      //encode contents into base64
-      const b64 = buffer.toString('base64')
-      const basePath = parse(localFilePath).base
-
-      const attachment: MailJetAttachment = {
-        Filename: basePath,
-        ContentType: mimeType,
-        Base64Content: b64
-      }
-      return attachment
     }
   )
 
-  return attachments
+  return Promise.all(attachments)
 }
 
 export {attach}
