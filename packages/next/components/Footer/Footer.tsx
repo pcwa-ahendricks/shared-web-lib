b0a48b0a6d6d9104883dfed20891ce5527a0e427
pcwa-ahendricks <ahendricks@pcwa.net>
2019-07-19 17:07:54 -0700

    ...

--- packages/next/components/Footer/Footer.tsx
+++ packages/next/components/Footer/Footer.tsx
@@ -13,6 +13,9 @@ const useStyles = makeStyles((theme: Theme) =>
     link: {
       color: theme.palette.grey[200],
       fontSize: '0.9rem'
+    },
+    foo: {
+      backgroundColor: 'black'
     }
   })
 )
@@ -22,27 +25,34 @@ const Footer = () => {
   const theme = useTheme<Theme>()
 
   return (
-    <Box bgcolor={theme.palette.primary.dark} height={theme.spacing(3)}>
-      <Box padding={2} bgcolor="inherit">
-        <Type variant="body2" component="span" className={classes.subtle}>
-          Copyright &copy; 2019
-        </Type>
-        <NextLink href="/" className={classes.link}>
-          &nbsp;&nbsp;Placer County Water Agency
-        </NextLink>
-        <Type variant="body2" component="span" className={classes.subtle}>
-          &nbsp;&nbsp;All Rights Reserved&nbsp;&nbsp;&#9830;&nbsp;&nbsp;Weather
-          Provided by
-        </Type>
-        <Link
-          variant="body2"
-          className={classes.link}
-          href="https://darksky.net/poweredby"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          &nbsp;Dark Sky
-        </Link>
+    <Box>
+      <Box bgcolor={theme.palette.primary.main}>
+        <Box p={3} bgcolor="inherit">
+          Foot
+        </Box>
+      </Box>
+      <Box bgcolor={theme.palette.primary.dark}>
+        <Box p={3} bgcolor="inherit">
+          <Type variant="body2" component="span" className={classes.subtle}>
+            Copyright &copy; 2019
+          </Type>
+          <NextLink href="/" className={classes.link}>
+            &nbsp;&nbsp;Placer County Water Agency
+          </NextLink>
+          <Type variant="body2" component="span" className={classes.subtle}>
+            &nbsp;&nbsp;All Rights
+            Reserved&nbsp;&nbsp;&#9830;&nbsp;&nbsp;Weather Provided by
+          </Type>
+          <Link
+            variant="body2"
+            className={classes.link}
+            href="https://darksky.net/poweredby"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &nbsp;Dark Sky
+          </Link>
+        </Box>
       </Box>
     </Box>
   )
