5b100103d4de1e2e8732b96a459662e1b6854901
pcwa-ahendricks <ahendricks@pcwa.net>
2019-07-22 15:00:42 -0700

    Fix flex property value.

--- packages/next/components/Footer/Footer.tsx
+++ packages/next/components/Footer/Footer.tsx
@@ -73,7 +73,7 @@ const Footer = () => {
           <RowBox
             bgcolor="inherit"
             flexDirection={{xs: 'column', sm: 'row'}}
-            alignItems={{xs: 'center', sm: 'initial'}}
+            alignItems={{xs: 'center', sm: 'flex-start'}}
           >
             <Box flex={{xs: '0 0 auto', sm: '1 1 25%'}}>
               <PcwaLogo
