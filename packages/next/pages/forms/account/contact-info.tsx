71ef2c59f7c48383cfd9e651fe77bf98feb9ac54
pcwa-ahendricks <ahendricks@pcwa.net>
2019-08-07 10:11:55 -0700

    Include last four ss field in form.

--- packages/next/pages/forms/account/contact-info.tsx
+++ packages/next/pages/forms/account/contact-info.tsx
@@ -295,6 +295,14 @@ const ContactInfo = () => {
                               component={AccountNoField}
                             />
                           </Grid>
+                          <Grid item xs={12} sm={5}>
+                            <Field
+                              name="lastFourSS"
+                              component={FormTextField}
+                              label="Last 4 Digits Social Security"
+                              required={false}
+                            />
+                          </Grid>
                           {/* <Grid item xs={12} sm={5}>
                             <Field
                               name="propertyType"
