c2c8790524523fd5e776044fd1c6718568fd7b9a
abe <ahendricks@pcwa.net>
2019-09-02 12:47:01 -0700

    Styling changes for <PrimaryHeader/>.

--- packages/next/components/HeaderContainer/HeaderContainer.tsx
+++ packages/next/components/HeaderContainer/HeaderContainer.tsx
@@ -1,36 +1,15 @@
-import React, {useState, useCallback} from 'react'
-import Sticky from 'react-sticky-el'
-import {makeStyles} from '@material-ui/styles'
+import React from 'react'
 import SecondaryHeader from '../SecondaryHeader/SecondaryHeader'
 import PrimaryHeader from '../PrimaryHeader/PrimaryHeader'
 import {Hidden} from '@material-ui/core'
 
-const useStyles = makeStyles({
-  sticky: {
-    zIndex: 1
-  }
-})
-
 const HeaderContainer = () => {
-  const classes = useStyles()
-  const [parentFixed, setParentFixed] = useState<boolean>()
-
-  const fixedToggleHandler = useCallback((wasFixed: boolean) => {
-    if (wasFixed) {
-      setParentFixed(false)
-    } else {
-      setParentFixed(true)
-    }
-  }, [])
-
   return (
     <React.Fragment>
       <Hidden only="xs" implementation="css">
         <SecondaryHeader />
       </Hidden>
-      <Sticky onFixedToggle={fixedToggleHandler} className={classes.sticky}>
-        <PrimaryHeader parentFixed={parentFixed} />
-      </Sticky>
+      <PrimaryHeader />
     </React.Fragment>
   )
 }
