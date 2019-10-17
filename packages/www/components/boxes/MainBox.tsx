824689e34f281eae0768f7c7e253eb19607e3c6c
pcwa-ahendricks <ahendricks@pcwa.net>
2019-10-16 17:02:00 -0700

    Changed <MainBox/> margins.

--- packages/www/components/boxes/MainBox.tsx
+++ packages/www/components/boxes/MainBox.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import {Box} from '@material-ui/core'
+import React, {useMemo} from 'react'
+import {Box, useMediaQuery} from '@material-ui/core'
+import {useTheme} from '@material-ui/core/styles'
 import {BoxProps} from '@material-ui/core/Box'
 
 type Props = {children?: React.ReactNode} & BoxProps
 
 const MainBox = ({children, ...rest}: Props) => {
+  const theme = useTheme()
+  const isSMUp = useMediaQuery(theme.breakpoints.up('sm'))
+  const marginTop = useMemo(() => (isSMUp ? 4 : 1), [isSMUp])
   return (
-    <Box component="main" mt={5} mb={5} {...rest}>
+    <Box component="main" mt={marginTop} mb={4} {...rest}>
       {children}
     </Box>
   )
