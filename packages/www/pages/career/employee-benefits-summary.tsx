ba0e5991f37123bd57fd66d79c35f0376758a53b
pcwa-ahendricks <ahendricks@pcwa.net>
2020-01-28 15:17:24 -0800

    Updated Water Systems MOU. (N. Skarda)

--- packages/www/pages/career/employee-benefits-summary.tsx
+++ packages/www/pages/career/employee-benefits-summary.tsx
@@ -142,7 +142,7 @@ const EmployeeBenefitsSummaryPage = () => {
                     <ListItemText primary="PCWA Power System MOU" />
                   </ListItemLink>
                   {/* <Divider /> */}
-                  <ListItemLink href="https://cdn.cosmicjs.com/dda1c2c0-fc06-11e9-92d1-516d3eb9feef-Water-Systems-MOU.pdf">
+                  <ListItemLink href="https://cdn.cosmicjs.com/20add320-4224-11ea-8470-89e2c1dd133a-Water-Systems-MOU.pdf">
                     <ListItemText primary="PCWA Water Systems MOU" />
                   </ListItemLink>
                 </List>
