25c2b437d5de0dc38a293623a2c35d57f3015c70
pcwa-ahendricks <ahendricks@pcwa.net>
2020-04-03 18:12:13 -0700

    Comment added.

--- packages/www/public/static/scripts/iframeResizerOpts.js
+++ packages/www/public/static/scripts/iframeResizerOpts.js
@@ -1,6 +1,6 @@
 // See https://github.com/davidjbradshaw/iframe-resizer/blob/master/docs/iframed_page/options.md for more info.
 // www.pcwa.net is the only site that should be mimicking dev-web page.
-// 'bodyScroll' is the only method that worked correctly when testing.
+// 'bodyScroll' is the only method that worked correctly when testing. This will override heightCalculationMethod provided by parent page.
 window.iFrameResizer = {
   targetOrigin: 'https://www.pcwa.net',
   heightCalculationMethod: 'bodyScroll'
