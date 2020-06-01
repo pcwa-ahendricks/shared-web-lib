c22e6102fd58a7a9e2ed05dba71c53469ea88928
abe <moemanchu@icloud.com>
2020-06-01 08:10:49 -0700

    Use next-transpile-modules with swr package.

--- next.config.js
+++ next.config.js
@@ -3,66 +3,66 @@ const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const {
   WebpackBundleSizeAnalyzerPlugin
 } = require('webpack-bundle-size-analyzer')
+// Fix error with IE11 and swr.
+const withTM = require('next-transpile-modules')(['swr']) // Pass the modules you would like to see transpiled
+const withPlugins = require('next-compose-plugins')
 const {STATS} = process.env
 
-module.exports = (_phase, {defaultConfig}) => {
-  return withBundleAnalyzer({
-    ...defaultConfig,
-    target: process.env.NEXT_TARGET,
-    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html'
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html'
-      }
+module.exports = withPlugins([withBundleAnalyzer, withTM], {
+  target: process.env.NEXT_TARGET,
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html'
     },
-    webpack: (config) => {
-      // Fixes npm packages that depend on `fs` module, as seen on https://github.com/zeit/now-examples/blob/master/nextjs/next.config.js
-      config.node = {
-        fs: 'empty'
-      }
-
-      // Polyfills - https://github.com/zeit/next.js/tree/master/examples/with-polyfills
-      const originalEntry = config.entry
-      config.entry = async () => {
-        const entries = await originalEntry()
-
-        if (
-          entries['main.js'] &&
-          !entries['main.js'].includes('./src/client/polyfills.js')
-        ) {
-          entries['main.js'].unshift('./src/client/polyfills.js')
-        }
-
-        return entries
-      }
-      //.
-
-      // Webpack Bundle Size Analyzer - https://github.com/zeit/next.js/tree/master/examples/with-webpack-bundle-size-analyzer
-      if (STATS) {
-        config.plugins.push(new WebpackBundleSizeAnalyzerPlugin('stats.txt'))
-      }
-
-      /**
-       * Fix Mapbox GL JS in production. See https://github.com/mapbox/mapbox-gl-js/issues/4348 for more info.
-       */
-      config.module = {
-        ...config.module,
-        noParse: /(mapbox-gl)\.js$/
-      }
-
-      // Use raw loader for markdown files.
-      config.module.rules.push({
-        test: /\.md$/,
-        loader: 'raw-loader'
-      })
-
-      return config
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html'
     }
-  })
-}
+  },
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module, as seen on https://github.com/zeit/now-examples/blob/master/nextjs/next.config.js
+    config.node = {
+      fs: 'empty'
+    }
+
+    // Polyfills - https://github.com/zeit/next.js/tree/master/examples/with-polyfills
+    const originalEntry = config.entry
+    config.entry = async () => {
+      const entries = await originalEntry()
+
+      if (
+        entries['main.js'] &&
+        !entries['main.js'].includes('./src/client/polyfills.js')
+      ) {
+        entries['main.js'].unshift('./src/client/polyfills.js')
+      }
+
+      return entries
+    }
+    //.
+
+    // Webpack Bundle Size Analyzer - https://github.com/zeit/next.js/tree/master/examples/with-webpack-bundle-size-analyzer
+    if (STATS) {
+      config.plugins.push(new WebpackBundleSizeAnalyzerPlugin('stats.txt'))
+    }
+
+    /**
+     * Fix Mapbox GL JS in production. See https://github.com/mapbox/mapbox-gl-js/issues/4348 for more info.
+     */
+    config.module = {
+      ...config.module,
+      noParse: /(mapbox-gl)\.js$/
+    }
+
+    // Use raw loader for markdown files.
+    config.module.rules.push({
+      test: /\.md$/,
+      loader: 'raw-loader'
+    })
+
+    return config
+  }
+})
