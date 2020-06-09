ac26fcda8149b1b7bb46142d1daef8ca2b75cc1c
Abe Hendricks <ahendricks@pcwa.net>
2020-06-09 16:44:20 -0700

    Comment added.

--- src/components/typography/StrongEmphasis/StrongEmphasis.tsx
+++ src/components/typography/StrongEmphasis/StrongEmphasis.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Typography as Type, TypographyProps} from '@material-ui/core'
+// Font loading using @font-face seems to work with css modules but I don't see where that's documented online. See https://nextjs.org/docs/basic-features/built-in-css-support for more info.
 import './StrongEmphasis.module.css'
 
 const StrongEmphasis = ({children, ...rest}: TypographyProps) => {
