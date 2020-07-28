292f069c3663d176762a1557b18cd97a41c47b88
Abe Hendricks <ahendricks@pcwa.net>
2020-07-28 09:19:08 -0700

    Fix typo

--- src/pages/resource-library/[...multimedia].tsx
+++ src/pages/resource-library/[...multimedia].tsx
@@ -459,7 +459,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
         // tabIndex,
         params
         // params: query
-      }
+      },
       revalidate: 5
     }
   } catch (error) {
