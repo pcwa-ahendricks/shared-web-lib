9f7b6ddc260e884fe188b9829d29f2fa19bd576a
Abe Hendricks <ahendricks@pcwa.net>
2020-08-25 17:00:45 -0700

    Fetch lqip in getStaticProps

---
+++ src/lib/then-filereader.ts
@@ -0,0 +1,15 @@
+export default function readAsDataUrlAsync(inputFile: Blob) {
+  const temporaryFileReader = new FileReader()
+
+  return new Promise((resolve, reject) => {
+    temporaryFileReader.onerror = () => {
+      temporaryFileReader.abort()
+      reject(new DOMException('Problem parsing input file'))
+    }
+
+    temporaryFileReader.onload = () => {
+      resolve(temporaryFileReader.result)
+    }
+    temporaryFileReader.readAsDataURL(inputFile)
+  })
+}
