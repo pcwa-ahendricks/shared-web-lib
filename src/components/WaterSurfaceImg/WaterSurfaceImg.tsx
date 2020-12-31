00db440e57ac8c2d854fb9b7c938c3d1709a708f
pcwa-ahendricks <ahendricks@pcwa.net>
2020-12-31 11:26:55 -0800

    Updated board image. (L. Young) Use next Image with banner.

--- src/components/WaterSurfaceImg/WaterSurfaceImg.tsx
+++ src/components/WaterSurfaceImg/WaterSurfaceImg.tsx
@@ -1,7 +1,7 @@
 // cspell:ignore imgix's
 import React from 'react'
-import {buildURL} from 'react-imgix'
-import {makeStyles, Box} from '@material-ui/core'
+import {Box} from '@material-ui/core'
+import Image from 'next/image'
 
 /**
  * Notes:
@@ -12,22 +12,30 @@ import {makeStyles, Box} from '@material-ui/core'
  * apply here since this image will always be visible due to it's position at the top and using a responsive image here is not needed since it's absolutely positioned.
  */
 
-const IMAGE_URL =
-  '//cosmic-s3.imgix.net/80212870-8e98-11e8-b9a6-bb6cb34bfd65-bg-water-surface_80.jpg'
-const HEIGHT = 100
-
-const useStyles = makeStyles({
-  root: {
-    backgroundPosition: 'top left',
-    backgroundRepeat: 'repeat-x',
-    backgroundImage: `url(${buildURL(IMAGE_URL, {h: HEIGHT})})`,
-    overflow: 'hidden'
-  }
-})
+// const useStyles = makeStyles({
+//   root: {
+//     backgroundPosition: 'top left',
+//     backgroundRepeat: 'repeat-x',
+//     backgroundImage: `url(${buildURL(IMAGE_URL, {h: HEIGHT})})`,
+//     overflow: 'hidden'
+//   }
+// })
 
 const WaterSurfaceImg = () => {
-  const classes = useStyles()
-  return <Box className={classes.root} height={HEIGHT} minHeight={HEIGHT} />
+  return (
+    <Box position="relative">
+      <Box height={100} width="100vw" zIndex={-1} overflow="hidden">
+        <Image
+          layout="fill"
+          objectFit="cover"
+          src="https://cosmic-s3.imgix.net/80212870-8e98-11e8-b9a6-bb6cb34bfd65-bg-water-surface_80.jpg"
+          alt="Drinking water surface background image"
+          priority
+        />
+      </Box>
+      .{' '}
+    </Box>
+  )
 }
 
 export default WaterSurfaceImg
