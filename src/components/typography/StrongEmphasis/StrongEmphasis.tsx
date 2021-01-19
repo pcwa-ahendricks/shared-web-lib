e4f40ed3189bba3da2f47a586e0dc811075cf15f
Abe Hendricks <ahendricks@pcwa.net>
2021-01-19 09:37:17 -0800

    Use named class with <StrongEmphasis/>

--- src/components/typography/StrongEmphasis/StrongEmphasis.tsx
+++ src/components/typography/StrongEmphasis/StrongEmphasis.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import {Typography as Type, TypographyProps} from '@material-ui/core'
 // Font loading using @font-face seems to work with css modules but I don't see where that's documented online. See https://nextjs.org/docs/basic-features/built-in-css-support for more info.
-import './StrongEmphasis.module.css'
+import styles from './StrongEmphasis.module.css'
 
 const StrongEmphasis = ({children, ...rest}: TypographyProps) => {
   return (
-    <Type color="inherit" variant="inherit" {...rest}>
+    <Type
+      color="inherit"
+      variant="inherit"
+      className={styles.strongEm}
+      {...rest}
+    >
       <strong>
         <em>{children}</em>
       </strong>
