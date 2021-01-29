3b67f5c817c6a35df5f7982d27ac7e0f07038895
Abe Hendricks <ahendricks@pcwa.net>
2021-01-29 14:10:20 -0800

    Better Novus Agenda fix

--- src/components/NovusIframe/NovusIframe.tsx
+++ src/components/NovusIframe/NovusIframe.tsx
@@ -1,19 +1,53 @@
 // cspell:ignore novus
-import React from 'react'
-import {Box, BoxProps} from '@material-ui/core'
+import React, {useCallback, useState} from 'react'
+import {
+  Typography as Type,
+  Box,
+  BoxProps,
+  makeStyles,
+  createStyles
+} from '@material-ui/core'
 import IeOnly from '@components/boxes/IeOnly'
 import IeNever from '@components/boxes/IeNever'
+import Animate from '@components/Animate/Animate'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    loadingCaption: {
+      userSelect: 'none',
+      pointerEvents: 'none' // This is important when using z-index. Certain web browsers will require this in order to select any elements beneath.
+    }
+  })
+)
 
 const NovusIframe = ({...rest}: BoxProps) => {
   // const theme = useTheme()
+  const classes = useStyles()
+  const [iframeIsLoading, setIframeIsLoading] = useState(true)
+
+  const novusIframeLoadedHandler = useCallback(() => {
+    setIframeIsLoading(false)
+  }, [])
 
   return (
     <Box position="relative" width="100%">
-      <Box
-        // name="fadeIn"
-        // animate={!iframeIsLoading}
-        // hideUntilAnimate
-        // speed="fast"
+      <Animate
+        name="fadeOut"
+        animate={!iframeIsLoading}
+        position="absolute"
+        speed="fast"
+        top={0}
+        left={0}
+        zIndex={1}
+        className={classes.loadingCaption}
+      >
+        <Type>Novus Agenda is loading...</Type>
+      </Animate>
+      <Animate
+        name="fadeIn"
+        animate={!iframeIsLoading}
+        hideUntilAnimate
+        speed="fast"
         height={{xs: 1400, md: 1250, lg: 1050}}
       >
         <IeNever
@@ -24,6 +58,7 @@ const NovusIframe = ({...rest}: BoxProps) => {
         >
           <iframe
             title="Novus Agenda"
+            onLoad={novusIframeLoadedHandler}
             src="https://pcwa.novusagenda.com/agendapublic/meetingsresponsive.aspx"
             frameBorder="0"
             width="100%"
@@ -31,11 +66,12 @@ const NovusIframe = ({...rest}: BoxProps) => {
             scrolling="auto"
           />
         </IeNever>
-      </Box>
+      </Animate>
       {/* iframe onLoad doesn't work with IE */}
       <IeOnly height="inherit" {...rest}>
         <iframe
           title="Novus Agenda"
+          onLoad={novusIframeLoadedHandler}
           src="https://pcwa.novusagenda.com/agendapublic/meetingsresponsive.aspx"
           frameBorder="0"
           width="100%"
