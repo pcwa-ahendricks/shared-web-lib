acd2dedac4ffc86697f71c8381f54f1e728df54d
Abe Hendricks <ahendricks@pcwa.net>
2021-05-10 15:37:20 -0700

    Fix year (J. Foster)

--- src/pages/about-pcwa/unclaimed-property.tsx
+++ src/pages/about-pcwa/unclaimed-property.tsx
@@ -69,7 +69,7 @@ const UnclaimedPropertyPage = ({initialData}: Props) => {
             <Type paragraph>
               Placer County Water Agency hereby provides notice to owners of
               record of unclaimed money in the Agency's possession that any
-              unclaimed money from the 2021 Calendar Year will escheat to the
+              unclaimed money from the 2017 Calendar Year will escheat to the
               Agency by operation of law if not claimed by{' '}
               <em>
                 {format(deadlineDate, "h':'mm aaaa 'on' cccc MMMM do',' yyyy")}
