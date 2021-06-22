a1a3cad4291a46b81c7f5d45d1bf76142692f778
Abe Hendricks <ahendricks@pcwa.net>
2021-06-22 14:07:11 -0700

    Added fire wise landscaping page (C. Kohn)

---
+++ src/pages/smart-water-use/fire-wise-landscaping.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+  Box,
+  Typography as Type,
+  createStyles,
+  makeStyles
+} from '@material-ui/core'
+import PageLayout from '@components/PageLayout/PageLayout'
+import MainBox from '@components/boxes/MainBox'
+import NarrowContainer from '@components/containers/NarrowContainer'
+
+const useStyles = makeStyles(() => createStyles({}))
+
+const FireWiseLandscapingPage = () => {
+  const classes = useStyles()
+  return (
+    <PageLayout title="Fire-wise Landscaping" waterSurface>
+      <MainBox>
+        <NarrowContainer>
+          <Box>foo</Box>
+        </NarrowContainer>
+      </MainBox>
+    </PageLayout>
+  )
+}
+
+export default FireWiseLandscapingPage
