1a5924236da5ad4d384e12782b58ac5f2926cc00
Abe Hendricks <ahendricks@pcwa.net>
2021-07-14 15:44:28 -0700

    Removed NEXT_PUBLIC_USE_NG_IFRAME env variable

--- public/static/scripts/iframeResizerOpts.js
+++
@@ -1,7 +0,0 @@
-// See https://github.com/davidjbradshaw/iframe-resizer/blob/master/docs/iframed_page/options.md for more info.
-// www.pcwa.net is the only site that should be mimicking dev-web page.
-// 'bodyScroll' is the only method that worked correctly when testing. This will override heightCalculationMethod provided by parent page.
-window.iFrameResizer = {
-  targetOrigin: 'https://www.pcwa.net',
-  heightCalculationMethod: 'bodyScroll'
-}
