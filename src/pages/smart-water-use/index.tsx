9581c4859ec9121ea80583d6ddf4296710bcd23e
Abe Hendricks <ahendricks@pcwa.net>
2021-07-15 16:02:19 -0700

    Switch columns on smart water use page

--- src/pages/smart-water-use/index.tsx
+++ src/pages/smart-water-use/index.tsx
@@ -134,6 +134,64 @@ const SmartWaterUsePage = () => {
           <RowBox responsive flexSpacing={6}>
             <ChildBox flex="50.00%">
               <Type variant="h4" color="primary">
+                Actions <strong>PCWA</strong> is taking
+              </Type>
+              <List dense disablePadding>
+                <BulletItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText primary="Operating PCWA’s western Placer groundwater wells to reduce surface water demands." />
+                </BulletItem>
+                <BulletItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText primary="Shifting a portion of wholesale demands to groundwater." />
+                </BulletItem>{' '}
+                <BulletItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText primary="Enhancing water efficiency rebate program." />
+                </BulletItem>
+                <BulletItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText primary="Expanding canal operation hours to monitoring for and minimize losses." />
+                </BulletItem>
+                <BulletItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText primary="Meeting our Water Forum commitment, to the lower American River, by releasing extra water from our reservoirs for fishery benefits." />
+                </BulletItem>
+              </List>
+              <Spacing size="large" />
+              <RowBox justifyContent="center" flexSpacing={2}>
+                <ChildBox>
+                  <WaterIcon fontSize="large" color="primary" />
+                </ChildBox>
+                <ChildBox>
+                  <EcoIcon fontSize="large" color="secondary" />
+                </ChildBox>
+              </RowBox>
+              <Spacing size="large" />
+              <Box width="90%" m="auto">
+                <Image
+                  src="aa2bd830-d0f0-11ea-95a6-2fa651cba029-PCWAQWEL-Certified-EmployeeWater-Efficiency.jpg"
+                  loader={imgixLoader}
+                  alt="PCWA Water Efficiency Technician adjusting sprinkle nozzle head."
+                  height={4480}
+                  width={6720}
+                  layout="responsive"
+                  sizes="(max-width: 600px) 90vw, 40vw"
+                />
+              </Box>
+            </ChildBox>
+            <ChildBox flex="50.00%">
+              <Type variant="h4" color="primary">
                 Actions <strong>customers</strong> can take outdoors and indoors
               </Type>
               <List dense disablePadding>
@@ -193,66 +251,8 @@ const SmartWaterUsePage = () => {
                 </BulletItem>
               </List>
             </ChildBox>
-            <ChildBox flex="50.00%">
-              <Type variant="h4" color="primary">
-                Actions <strong>PCWA</strong> is taking
-              </Type>
-              <List dense disablePadding>
-                <BulletItem>
-                  <ListItemBullet>
-                    <BulletIcon fontSize="large" />
-                  </ListItemBullet>
-                  <ListItemText primary="Operating PCWA’s western Placer groundwater wells to reduce surface water demands." />
-                </BulletItem>
-                <BulletItem>
-                  <ListItemBullet>
-                    <BulletIcon fontSize="large" />
-                  </ListItemBullet>
-                  <ListItemText primary="Shifting a portion of wholesale demands to groundwater." />
-                </BulletItem>{' '}
-                <BulletItem>
-                  <ListItemBullet>
-                    <BulletIcon fontSize="large" />
-                  </ListItemBullet>
-                  <ListItemText primary="Enhancing water efficiency rebate program." />
-                </BulletItem>
-                <BulletItem>
-                  <ListItemBullet>
-                    <BulletIcon fontSize="large" />
-                  </ListItemBullet>
-                  <ListItemText primary="Expanding canal operation hours to monitoring for and minimize losses." />
-                </BulletItem>
-                <BulletItem>
-                  <ListItemBullet>
-                    <BulletIcon fontSize="large" />
-                  </ListItemBullet>
-                  <ListItemText primary="Meeting our Water Forum commitment, to the lower American River, by releasing extra water from our reservoirs for fishery benefits." />
-                </BulletItem>
-              </List>
-              <Spacing size="large" />
-              <RowBox justifyContent="center" flexSpacing={2}>
-                <ChildBox>
-                  <WaterIcon fontSize="large" color="primary" />
-                </ChildBox>
-                <ChildBox>
-                  <EcoIcon fontSize="large" color="secondary" />
-                </ChildBox>
-              </RowBox>
-              <Spacing size="large" />
-              <Box width="90%" m="auto">
-                <Image
-                  src="aa2bd830-d0f0-11ea-95a6-2fa651cba029-PCWAQWEL-Certified-EmployeeWater-Efficiency.jpg"
-                  loader={imgixLoader}
-                  alt="PCWA Water Efficiency Technician adjusting sprinkle nozzle head."
-                  height={4480}
-                  width={6720}
-                  layout="responsive"
-                  sizes="(max-width: 600px) 90vw, 40vw"
-                />
-              </Box>
-            </ChildBox>
           </RowBox>
-          <Spacing factor={2} />
+          <Spacing factor={2} size="large" />
           <RowBox responsive flexSpacing={6}>
             <ChildBox flex="66.66%">
               <Type variant="h3" color="primary">
