79cfac32e1d591a65345ad7eaa3cf99451f457c5
Abe Hendricks <ahendricks@pcwa.net>
2021-09-02 10:41:08 -0700

    Added photos to page (R. Branch)

--- src/pages/smart-water-use/mulch-mayhem.tsx
+++ src/pages/smart-water-use/mulch-mayhem.tsx
@@ -10,12 +10,13 @@ import {
 } from '@material-ui/core'
 import PageLayout from '@components/PageLayout/PageLayout'
 import MainBox from '@components/boxes/MainBox'
-import NarrowContainer from '@components/containers/NarrowContainer'
 import PageTitle from '@components/PageTitle/PageTitle'
 import Image from 'next/image'
 import imgixLoader from '@lib/imageLoader'
 import Spacing from '@components/boxes/Spacing'
 import BulletIcon from 'mdi-material-ui/CircleSmall'
+import WideContainer from '@components/containers/WideContainer'
+import {ChildBox, ColumnBox, RowBox} from 'mui-sleazebox'
 
 const useStyles = makeStyles((theme) => ({
   // [HACK] - This transition is required to prevent the animation from flickering back on after animation. Not sure why it's flickering at all. This doesn't stop the flicker, merely makes the image transparent so that it is not seen.
@@ -47,7 +48,7 @@ export default function MulchMayhemPage() {
   return (
     <PageLayout title="Mulch Mayhem" waterSurface>
       <MainBox>
-        <NarrowContainer>
+        <WideContainer>
           <PageTitle title="Mulch Mayhem" />
           <Image
             src="3f897b20-0b70-11ec-93a7-070c59f98950-MulchMayhemWebsiteBanner2.jpg"
@@ -58,55 +59,99 @@ export default function MulchMayhemPage() {
             height={1075}
             className={classes.imageLink}
           />
-          <Spacing />
-          <Type variant="h2" color="primary" gutterBottom>
-            <em>
-              Free Mulch available for Drive Thru Customers of hosting agencies
-            </em>
-          </Type>
-          <Spacing />
-          <Type>
-            Join us for Mulch Mayhem{' '}
-            <strong>Saturday, September 25th from 8:00 am - 12:00 pm</strong> at
-            Sierra College Overflow Lot, located at the corner of Rocklin Road
-            and El Don Drive.
-          </Type>
-          <List>
-            <ListItem>
-              <ListItemBullet>
-                <BulletIcon fontSize="large" />
-              </ListItemBullet>
-              <ListItemText
-                primary="A Drive Thru Only for Trucks and Trailers. (Bring your own tarp
+          <Spacing factor={2} size="large" />
+          <RowBox flexSpacing={10}>
+            <ChildBox flex="60%">
+              <Type variant="h2" color="primary" gutterBottom>
+                <em>
+                  Free Mulch available for Drive Thru Customers of hosting
+                  agencies
+                </em>
+              </Type>
+              <Spacing />
+              <Type>
+                Join us for Mulch Mayhem{' '}
+                <strong>
+                  Saturday, September 25th from 8:00 am - 12:00 pm
+                </strong>{' '}
+                at Sierra College Overflow Lot, located at the corner of Rocklin
+                Road and El Don Drive.
+              </Type>
+              <List>
+                <ListItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText
+                    primary="A Drive Thru Only Event for Trucks and Trailers. (Bring your own tarp
                 to cover)"
-              />
-            </ListItem>
-            <ListItem>
-              <ListItemBullet>
-                <BulletIcon fontSize="large" />
-              </ListItemBullet>
-              <ListItemText primary="Provided on a First-come, first-served basis until supplies are gone." />
-            </ListItem>
-            <ListItem>
-              <ListItemBullet>
-                <BulletIcon fontSize="large" />
-              </ListItemBullet>
-              <ListItemText primary="For personal use only; not for resale or commercial use." />
-            </ListItem>
-          </List>
-          <Spacing />
-          <Type variant="subtitle1" gutterBottom>
-            Mulch Conserves Water!
-          </Type>
-          <Type paragraph>
-            Mulch is like icing on a cake, because it keeps the soil moist the
-            way icing keeps a cake moist. Mulch slows evaporation allowing water
-            to sink into the soil, moderates soil temperature and breaks down
-            into nutrients for plants. Be sure to add three to four inches or
-            organic mulch (e.g. leaves, wood chips) around trees and plants for
-            the greatest benefit.
-          </Type>
-        </NarrowContainer>
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText primary="Provided on a First-come, first-served basis until supplies are gone." />
+                </ListItem>
+                <ListItem>
+                  <ListItemBullet>
+                    <BulletIcon fontSize="large" />
+                  </ListItemBullet>
+                  <ListItemText primary="For personal use only; not for resale or commercial use." />
+                </ListItem>
+              </List>
+              <Spacing />
+              <Type variant="subtitle1" gutterBottom>
+                Mulch Conserves Water!
+              </Type>
+              <Type paragraph>
+                Mulch is like icing on a cake, because it keeps the soil moist
+                the way icing keeps a cake moist. Mulch slows evaporation
+                allowing water to sink into the soil, moderates soil temperature
+                and breaks down into nutrients for plants. Be sure to add three
+                to four inches or organic mulch (e.g. leaves, wood chips) around
+                trees and plants for the greatest benefit.
+              </Type>
+            </ChildBox>
+            <ColumnBox child flex="40%">
+              <ChildBox>
+                <Image
+                  src="a80ea740-0c12-11ec-93a7-070c59f98950-Mulch-Mayhem-loading-trucks01.JPG"
+                  alt="Pickup truck with a bed full of mulch"
+                  layout="responsive"
+                  loader={imgixLoader}
+                  height={3840}
+                  width={5760}
+                  className={classes.imageLink}
+                />
+              </ChildBox>
+              <Spacing />
+              <ChildBox>
+                <Image
+                  src="a07660e0-0c12-11ec-93a7-070c59f98950-Mulch-Mayhem-loading-trucks04.JPG"
+                  alt="Tractor loading mulch into the bed of a pickup truck"
+                  layout="responsive"
+                  loader={imgixLoader}
+                  height={3840}
+                  width={5760}
+                  className={classes.imageLink}
+                />
+              </ChildBox>
+              <Spacing />
+              <ChildBox>
+                <Image
+                  src="99102700-0c12-11ec-93a7-070c59f98950-Mulch-Mayhem-loading-trucks02.JPG"
+                  alt="Tractor loading mulch"
+                  layout="responsive"
+                  loader={imgixLoader}
+                  height={3840}
+                  width={5760}
+                  className={classes.imageLink}
+                />
+              </ChildBox>
+            </ColumnBox>
+          </RowBox>
+        </WideContainer>
       </MainBox>
     </PageLayout>
   )
