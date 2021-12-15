6ed448f4da1d2dfcfd614a2ae0f30ccf7948dc9e
Abe Hendricks <ahendricks@pcwa.net>
2021-12-15 11:30:31 -0800

    log msg

--- src/components/boxes/IeNever.tsx
+++ src/components/boxes/IeNever.tsx
@@ -4,5 +4,6 @@ import useMatchesIe from '@hooks/useMatchesIe'
 
 export default function IeNever({children, ...props}: BoxProps) {
   const matches = useMatchesIe()
+  console.log('ieNever: ', matches)
   return matches ? null : <Box {...props}>{children}</Box>
 }
--- src/components/boxes/IeOnly.tsx
+++ src/components/boxes/IeOnly.tsx
@@ -4,5 +4,6 @@ import useMatchesIe from '@hooks/useMatchesIe'
 
 export default function IeOnly({children, ...props}: BoxProps) {
   const matchesIe = useMatchesIe()
+  console.log('ieOnly: ', matchesIe)
   return matchesIe ? <Box {...props}>{children}</Box> : null
 }
