44a7e5b9175a6e6be9606b651482a3108e39ecc9
Abe Hendricks <ahendricks@pcwa.net>
2022-03-01 09:44:43 -0800

    Rate adjust page edits for 2023 (R. Branch)

--- src/pages/services/rate-adjust.tsx
+++ src/pages/services/rate-adjust.tsx
@@ -16,104 +16,88 @@ const RateAdjustPage = () => {
       <MainBox>
         <WideContainer>
           <PageTitle
-            title="2018 Multiyear Rate Adjustment"
+            title="2023 Multiyear Rate Adjustment"
             subtitle="Services"
           />
-          <Type variant="h2" gutterBottom>
-            Background
-          </Type>
-          <Type paragraph>
-            At the November 16 meeting of the Placer County Water Agency (PCWA)
-            Board of Directors, the Board approved a resolution adopting new
-            rates, fees, and charges for water service throughout the Agency’s
-            Western Water system, also known as Zone 6. The Western Water system
-            encompasses all of PCWA’s current service area. The new rates, which
-            take effect January 1, 2018, were calculated using a cost-of-service
-            approach and are the result of a two-year rate study, conducted by
-            an independent rate consultant, which took into account Agency
-            revenue requirements and associated costs.
-          </Type>
 
-          <Type paragraph>
-            There are two reasons for the adjustment in rates. The first reason
-            is to maintain compliance with current law. In 2015, a California
-            appellate court ruled that tiered water rates must be based on the
-            actual cost of providing water service to each customer, rather than
-            an escalating scale based on the quantity of water used. To comply
-            with this legal finding, PCWA collapsed its seven-tier structure to
-            three tiers, for most rate structures. The second reason is related
-            to capital improvement projects. For the past four years, PCWA has
-            been using reserves to complete needed renewal and replacement
-            projects. In addition, operating costs continue to increase annually
-            as the cost of materials and supplies increase. The new rates
-            reflect the increased cost of updating, operating, and maintaining
-            PCWA’s water system.
-          </Type>
+          <RowBox responsive="sm" flexSpacing={6}>
+            <ChildBox flex="55%">
+              <Type variant="h3" gutterBottom>
+                Background
+              </Type>
+              <Type paragraph>
+                At the March 3 meeting of the Placer County Water Agency (PCWA)
+                Board of Directors, the Board approved a resolution adopting new
+                rates, fees, and charges for water service throughout the
+                Agency's Western Water system.. The new rates, which take effect
+                January 1, 2023, were calculated using a cost-of-service
+                approach and are the result of a two-year rate study, conducted
+                by an independent rate consultant, which considered Agency
+                revenue requirements and associated costs.
+              </Type>
 
-          <Type paragraph>
-            In order to adopt the new rates and charges, PCWA had to comply with
-            Proposition 218. The Board initiated the Proposition 218 process on
-            September 8, and concluded it with a public hearing on November 2.
-          </Type>
+              <Type paragraph>
+                In order to adopt the new rates and charges, PCWA must comply
+                with Proposition 218. The Board initiated the Proposition 218
+                process on March 3 and will complete the process with a public
+                hearing on May 9.
+              </Type>
+            </ChildBox>
+            <ChildBox flex="45%" flexShrink={0}>
+              <Box bgcolor={theme.palette.background.paper} p={3} boxShadow={1}>
+                <Type variant="subtitle1">
+                  Notice of Public Hearing Documents
+                </Type>
+                <RowBox justifyContent="space-around" mt={3}>
+                  <Box flex="33.33">
+                    <ImageThumbLink
+                      imgixUrl="https://cosmicjs.imgix.net/128d45c0-980b-11e7-899f-f5a4f2fb3548-2018 - Zone 6 Treated 218 notice - 2018 Final_090817.pdf"
+                      filename="PCWA Zone 6 Treated 218 notice - 2018.pdf"
+                      caption="Treated Water Notice"
+                      alt="Thumbnail and link for Prop. 218 Treated Water Notice"
+                      sizes="(max-width: 600px) 30vw, 15vw"
+                    />
+                  </Box>
+                  <Box flex="33.33">
+                    <ImageThumbLink
+                      imgixUrl="https://cosmicjs.imgix.net/5dfb28a0-980c-11e7-899f-f5a4f2fb3548-2018 - Zone 6 Untreated Water 218 notice - 2018 Final_090817.pdf"
+                      filename="PCWA Zone 6 Untreated 218 notice - 2018.pdf"
+                      caption="Untreated Water Notice"
+                      alt="Thumbnail and link for Prop. 218 Untreated Water Notice"
+                      sizes="(max-width: 600px) 30vw, 15vw"
+                    />
+                  </Box>
+                  <Box flex="33.33">
+                    <ImageThumbLink
+                      imgixUrl="https://imgix.cosmicjs.com/d796c8b0-fa7d-11e9-ac85-afda513db67b-PCWA-Cost-of-Service-Study---Final-Report.pdf"
+                      filename="PCWA Cost of Service Study.pdf"
+                      caption="2017 Cost of Service - Rate Study"
+                      alt="Thumbnail and link for Cost of Service Study"
+                      sizes="(max-width: 600px) 30vw, 15vw"
+                    />
+                  </Box>
+                </RowBox>
+
+                <Box mt={6}>
+                  <Type variant="subtitle1" gutterBottom>
+                    Resources Regarding Prop. 218
+                  </Type>
+                  <Link
+                    href="https://www.lao.ca.gov/1996/120196_prop_218/understanding_prop218_1296.html"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Understanding Proposition 218 - Legislative Analyst's Office
+                  </Link>
+                </Box>
+              </Box>
+            </ChildBox>
+          </RowBox>
 
           <Box mt={6}>
             <RowBox responsive flexSpacing={3}>
               <ChildBox>
                 <RateAdjustFAQ />
-              </ChildBox>
-              <ChildBox flex="50%" flexShrink={0}>
-                <Box
-                  bgcolor={theme.palette.background.paper}
-                  p={3}
-                  boxShadow={1}
-                >
-                  <Type variant="subtitle1">
-                    Notice of Public Hearing Documents
-                  </Type>
-                  <RowBox justifyContent="space-around" mt={3}>
-                    <Box flex="33.33">
-                      <ImageThumbLink
-                        imgixUrl="https://cosmicjs.imgix.net/128d45c0-980b-11e7-899f-f5a4f2fb3548-2018 - Zone 6 Treated 218 notice - 2018 Final_090817.pdf"
-                        filename="PCWA Zone 6 Treated 218 notice - 2018.pdf"
-                        caption="Treated Water Notice"
-                        alt="Thumbnail and link for Prop. 218 Treated Water Notice"
-                        sizes="(max-width: 600px) 30vw, 15vw"
-                      />
-                    </Box>
-                    <Box flex="33.33">
-                      <ImageThumbLink
-                        imgixUrl="https://cosmicjs.imgix.net/5dfb28a0-980c-11e7-899f-f5a4f2fb3548-2018 - Zone 6 Untreated Water 218 notice - 2018 Final_090817.pdf"
-                        filename="PCWA Zone 6 Untreated 218 notice - 2018.pdf"
-                        caption="Untreated Water Notice"
-                        alt="Thumbnail and link for Prop. 218 Untreated Water Notice"
-                        sizes="(max-width: 600px) 30vw, 15vw"
-                      />
-                    </Box>
-                    <Box flex="33.33">
-                      <ImageThumbLink
-                        imgixUrl="https://imgix.cosmicjs.com/d796c8b0-fa7d-11e9-ac85-afda513db67b-PCWA-Cost-of-Service-Study---Final-Report.pdf"
-                        filename="PCWA Cost of Service Study.pdf"
-                        caption="2017 Cost of Service – Rate Study"
-                        alt="Thumbnail and link for Cost of Service Study"
-                        sizes="(max-width: 600px) 30vw, 15vw"
-                      />
-                    </Box>
-                  </RowBox>
-
-                  <Box mt={6}>
-                    <Type variant="subtitle1" gutterBottom>
-                      Resources Regarding Prop. 218
-                    </Type>
-                    <Link
-                      href="https://www.lao.ca.gov/1996/120196_prop_218/understanding_prop218_1296.html"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      Understanding Proposition 218 – Legislative Analyst’s
-                      Office
-                    </Link>
-                  </Box>
-                </Box>
               </ChildBox>
             </RowBox>
           </Box>
