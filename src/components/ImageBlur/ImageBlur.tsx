46093d45c6e480eaa231b0ac2266ebbf668bd73d
Abe Hendricks <ahendricks@pcwa.net>
2022-07-13 12:09:22 -0700

    changes to <ImageBlur/>

--- src/components/ImageBlur/ImageBlur.tsx
+++ src/components/ImageBlur/ImageBlur.tsx
@@ -1,9 +1,10 @@
-import React, {useCallback, useMemo} from 'react'
+import React, {useCallback, useMemo, useState} from 'react'
 import {textFetcher} from '@lib/fetcher'
 import Image, {ImageProps} from 'next/image'
-import {decode} from 'blurhash'
+// import {decode} from 'blurhash'
 import {stringify} from 'querystringify'
 import imgixLoader from '@lib/imageLoader'
+import {BlurhashCanvas} from 'react-blurhash'
 
 interface Placeholder {
   url: string
@@ -16,10 +17,10 @@ const DEFAULT_HEIGHT = 50
 
 type Placeholders = Placeholder[]
 
-const toBase64 = (str: string) =>
-  typeof window === 'undefined'
-    ? Buffer.from(str).toString('base64')
-    : window.btoa(str)
+// const toBase64 = (str: string) =>
+//   typeof window === 'undefined'
+//     ? Buffer.from(str).toString('base64')
+//     : window.btoa(str)
 
 const getImgixBlurHash = async (
   filename: string,
@@ -50,56 +51,93 @@ type Props = {
   alt: string
   placeholders: Placeholders
   src: string
+  width: number
+  height: number
   blurWidth?: number
   blurHeight?: number
-} & Omit<ImageProps, 'src'>
+} & Omit<ImageProps, 'src' | 'number' | 'height'>
 
 const ImageBlur = ({
   alt,
   placeholders,
   blurHeight = DEFAULT_HEIGHT,
   blurWidth = DEFAULT_WIDTH,
+  width,
+  height,
   src,
   ...rest
 }: Props) => {
-  const getPlaceHolderB64 = useCallback(
-    (filename: string) => {
-      const idx = placeholders.findIndex((p) => p.filename === filename)
-      const d: unknown = decode(
-        placeholders[idx].blurhash,
-        blurWidth,
-        blurHeight
-      )
-      console.log('blurhashString', d)
-      return d as string
-    },
-    [placeholders, blurHeight, blurWidth]
-  )
+  // const getPlaceHolderB64 = useCallback(
+  //   (filename: string) => {
+  //     const idx = placeholders.findIndex((p) => p.filename === filename)
+  //     return decode(placeholders[idx].blurhash, blurWidth, blurHeight)
+  //   },
+  //   [placeholders, blurHeight, blurWidth]
+  // )
 
-  const blurDataURL = useMemo(() => {
-    const b64 = getPlaceHolderB64(src)
-    if (b64) {
-      return `data:image/jpg;base64,${toBase64(b64)}`
-    } else {
-      return null
-    }
-  }, [getPlaceHolderB64, src])
+  const hash = useMemo(() => {
+    const idx = placeholders.findIndex((p) => p.filename === src)
+    return placeholders[idx].blurhash
+  }, [placeholders, src])
 
-  if (blurDataURL) {
-    console.log('render blur image')
-    return (
+  // const blurDataURL = useMemo(() => {
+  //   const pixels = getPlaceHolderB64(src)
+  //   if (typeof window !== 'undefined') {
+  //     const canvas = document.createElement('canvas')
+  //     const ctx = canvas.getContext('2d')
+  //     const imageData = ctx?.createImageData(width, height)
+  //     imageData?.data.set(pixels)
+  //     if (imageData) {
+  //       ctx?.putImageData(imageData, 0, 0)
+  //       return canvas.toDataURL()
+  //     }
+  //   } else {
+  //     return null
+  //   }
+  // }, [getPlaceHolderB64, src, width, height])
+
+  const [loaded, setLoaded] = useState(false)
+
+  const loadedHandler = useCallback(() => {
+    setTimeout(() => setLoaded(true))
+  }, [])
+
+  // if (blurDataURL) {
+  return (
+    <div style={{position: 'relative'}}>
       <Image
         loader={imgixLoader}
         src={src}
-        placeholder="blur"
-        blurDataURL={blurDataURL}
+        // placeholder="blur"
+        // blurDataURL={blurDataURL}
+        width={width}
+        height={height}
         alt={alt}
+        onLoadingComplete={loadedHandler}
         {...rest}
       />
-    )
-  } else {
-    return <></>
-  }
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          width: '100%',
+          opacity: loaded ? 0 : 1,
+          transition: 'all 400ms'
+        }}
+      >
+        <BlurhashCanvas
+          hash={hash}
+          width={width}
+          height={height}
+          style={{width: 'inherit'}}
+          punch={1}
+        />
+      </div>
+    </div>
+  )
+  // } else {
+  //   return <></>
+  // }
 }
 
 export default ImageBlur
