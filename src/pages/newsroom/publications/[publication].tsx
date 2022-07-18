ffea36048b82598fd45dc5315f85e12ccfc761ce
Abe Hendricks <ahendricks@pcwa.net>
2022-07-18 16:59:16 -0700

    use image placeholders with publications

--- src/pages/newsroom/publications/[publication].tsx
+++ src/pages/newsroom/publications/[publication].tsx
@@ -68,8 +68,22 @@ import useSWR from 'swr'
 import {stringify} from 'querystringify'
 import fetcher from '@lib/fetcher'
 import imgixLoader, {imgixUrlLoader} from '@lib/imageLoader'
+import {getImgixBlurHashes} from '@components/imageBlur/ImageBlur'
+import usePlaceholders from '@components/imageBlur/usePlaceholders'
+import {Placeholders} from '@components/imageBlur/ImageBlurStore'
 
 const DATE_FNS_FORMAT = 'yyyy-MM-dd'
+
+const imgixImages = [
+  '21555930-fbc6-11eb-9a4d-af05155ef55d-FireWater8.5x11LOW2021.pdf',
+  'cced2b40-f2e1-11ea-a3de-692d5982216c-Fire--Water---2020.pdf',
+  '4ab2d130-7fd4-11ec-bfce-f334865cc167-2021YearEndReport.pdf',
+  '492aa9a0-6658-11eb-8120-dfe8ec2b682f-Year-End-Report-2020FINAL.pdf',
+  '1dc4d750-2b57-11ea-bfe8-5b62c3bdf959-2019-YEAR-END-REPORT-FINAL.pdf',
+  '61bcf350-104d-11e9-81dd-490e145a6cb6-2018-YEAR-END-REPORT---FINAL.pdf',
+  '0ce1e4c0-a6cd-11ec-8a0f-d90ff9705f55-2022-Strategic-Plan.pdf',
+  '7f7c0030-f4ab-11eb-af9b-23a6e756c49c-2021-Strategic-PlanFINAL.pdf'
+]
 
 interface EnewsBlastMetadata {
   mailchimpURL?: string
@@ -87,6 +101,7 @@ type Props = {
   initialEnewsBlasts?: CosmicObjectResponse<EnewsBlastMetadata>
   initialNewsletters?: CosmicMediaResponse
   err?: {statusCode: number}
+  placeholders?: Placeholders
 }
 
 export const cosmicFetcher = (
@@ -138,7 +153,8 @@ const PublicationsPage = ({
   err,
   initialEnewsBlasts,
   initialNewsletters,
-  publicationParam = ''
+  publicationParam = '',
+  placeholders
 }: Props) => {
   const classes = useStyles()
   const theme = useTheme()
@@ -150,6 +166,8 @@ const PublicationsPage = ({
   const isLGUp = useMediaQuery(theme.breakpoints.up('lg'))
   const isMDUp = useMediaQuery(theme.breakpoints.up('md'))
   const isXS = useMediaQuery(theme.breakpoints.only('xs'))
+
+  usePlaceholders(placeholders)
 
   const {data: newslettersData} = useSWR<CosmicMediaResponse>(newslettersUrl, {
     fallbackData: initialNewsletters
@@ -760,9 +778,15 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
         'id,metadata,status,title'
       )
     ])
-
+    // Placeholder images
+    const placeholders = await getImgixBlurHashes(imgixImages)
     return {
-      props: {initialNewslettersData, publicationParam, initialEnewsBlasts},
+      props: {
+        initialNewslettersData,
+        publicationParam,
+        initialEnewsBlasts,
+        placeholders
+      },
       revalidate: 5
     }
   } catch (error) {
