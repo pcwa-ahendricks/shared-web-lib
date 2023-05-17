4792276082346fa42b7b29b12c7b94d3af0f761c
Abe Hendricks <ahendricks@pcwa.net>
2023-05-17 16:43:10 -0700

    Added Summer Strong page (C. Kohn).

---
+++ src/pages/smart-water-use/summer-strong.tsx
@@ -0,0 +1,275 @@
+import React from 'react'
+import PageLayout from '@components/PageLayout/PageLayout'
+import MainBox from '@components/boxes/MainBox'
+import PageTitle from '@components/PageTitle/PageTitle'
+import {RowBox, ChildBox} from 'mui-sleazebox'
+import {Typography as Type, Box, Link} from '@material-ui/core'
+// import LeafIcon from 'mdi-material-ui/Leaf'
+import BirdIcon from 'mdi-material-ui/Bird'
+import Image from 'next/image'
+import NextLink from 'next/link'
+import imgixLoader from '@lib/imageLoader'
+import Spacing from '@components/boxes/Spacing'
+import ImageFancier from '@components/ImageFancier/ImageFancier'
+import {getImgixBlurHashes} from '@components/imageBlur/ImageBlur'
+import {GetStaticProps} from 'next'
+import usePlaceholders from '@components/imageBlur/usePlaceholders'
+import {Placeholders} from '@components/imageBlur/ImageBlurStore'
+import MediaDialogOnClick from '@components/MediaDialogOnClick/MediaDialogOnClick'
+import WideContainer from '@components/containers/WideContainer'
+import {blueGrey} from '@material-ui/core/colors'
+
+type Props = {
+  placeholders: Placeholders
+}
+
+const imgixImages = [
+  '29837ac0-f4d4-11ed-bb44-790a83f99a24-PCWA-Summer-Strong-Cleveland-Sage-050823.jpg',
+  '29536a10-f4d4-11ed-bb44-790a83f99a24-FL-Salvia-clevelandii-State-Fair-7-24.JPG',
+  '297bb290-f4d4-11ed-bb44-790a83f99a24-N-Salvia-clevelandii-8-23.JPG',
+  '296878b0-f4d4-11ed-bb44-790a83f99a24-N-Salvia-clevelandii-5-16-3.JPG'
+]
+
+export default function SummerStrongPage({placeholders}: Props) {
+  usePlaceholders(placeholders)
+
+  return (
+    <PageLayout title="Summer Strong" waterSurface>
+      <MainBox>
+        <WideContainer>
+          <PageTitle title="Summer Strong" subtitle="Smart Water Use" />
+          <RowBox responsive flexSpacing={4}>
+            <ChildBox flex="50%">
+              <Type paragraph>
+                Summer Strong plants are not only beautiful but tough enough to
+                handle the hottest days. PCWA is pleased to partner with the UC
+                Master Gardeners of Placer County to highlight six Summer Strong
+                flowers, shrubs and trees that are water-wise, well-suited to
+                Placer County's environment and available at local nurseries.
+              </Type>
+              <Type>
+                Fire-wise plants are also Summer Strong. You can learn more
+                about Fire-Wise landscaping, including a list of plants used in
+                the Maidu Fire Station Makeover in Auburn on our{' '}
+                <NextLink
+                  href="/smart-water-use/fire-wise-landscaping"
+                  passHref
+                >
+                  <Link underline="always">
+                    Fire-Wise, Water-Wise Landscaping
+                  </Link>
+                </NextLink>{' '}
+                page.
+              </Type>
+            </ChildBox>
+            <ChildBox
+              flex="40%"
+              mx="auto"
+              width={{xs: '50vw', sm: '100%'}} // Don't let portrait image get too big in small layouts.
+            >
+              <Image
+                src="29837ac0-f4d4-11ed-bb44-790a83f99a24-PCWA-Summer-Strong-Cleveland-Sage-050823.jpg"
+                alt="Cleveland Sage"
+                loader={imgixLoader}
+                layout="responsive"
+                sizes="(max-width: 600px) 60vw, 40vw"
+                width={1080}
+                height={1080}
+              />
+            </ChildBox>
+          </RowBox>
+          <Spacing size="x-large" />
+          <Box>
+            <Type variant="h2" color="primary">
+              Featured Plant of the Month
+            </Type>
+            <Spacing />
+            <RowBox>
+              {/* <LeafIcon color="secondary" style={{marginRight: 8}} /> */}
+              <BirdIcon
+                style={{marginRight: 8, color: blueGrey[500], fontSize: 32}}
+              />
+              <Type variant="h2" gutterBottom>
+                Hello Hummingbirds!
+              </Type>
+            </RowBox>
+            <Type variant="h3">Cleveland Sage (and other Salvias)</Type>
+            <Spacing size="large" />
+            <RowBox flexSpacing={6} responsive>
+              <ChildBox>
+                <MediaDialogOnClick
+                  mediaUrl="https://imgix.cosmicjs.com/29536a10-f4d4-11ed-bb44-790a83f99a24-FL-Salvia-clevelandii-State-Fair-7-24.JPG"
+                  mediaName="'Winnifred Gilman' Cleveland Sage, Salvia clevelandii 'Winnifred Gilman'"
+                  mediaPreviewDialogProps={{
+                    width: 1975 * 0.15,
+                    height: 2688 * 0.15
+                  }}
+                >
+                  <ImageFancier
+                    layout="intrinsic"
+                    alt="'Winnifred Gilman' Cleveland Sage, Salvia clevelandii 'Winnifred Gilman'"
+                    src="https://imgix.cosmicjs.com/29536a10-f4d4-11ed-bb44-790a83f99a24-FL-Salvia-clevelandii-State-Fair-7-24.JPG"
+                    width={1975 * 0.15}
+                    height={2688 * 0.15}
+                    defaultGrey
+                    sizes="50vw"
+                    objectFit="contain"
+                  />
+                </MediaDialogOnClick>
+              </ChildBox>
+              <ChildBox>
+                <MediaDialogOnClick
+                  mediaUrl="https://imgix.cosmicjs.com/297bb290-f4d4-11ed-bb44-790a83f99a24-N-Salvia-clevelandii-8-23.JPG"
+                  mediaName="'Winnifred Gilman' Cleveland Sage, Salvia clevelandii 'Winnifred Gilman'"
+                  mediaPreviewDialogProps={{
+                    width: 3939 * 0.1,
+                    height: 3456 * 0.1
+                  }}
+                >
+                  <ImageFancier
+                    layout="intrinsic"
+                    alt="'Winnifred Gilman' Cleveland Sage, Salvia clevelandii 'Winnifred Gilman'"
+                    src="https://imgix.cosmicjs.com/297bb290-f4d4-11ed-bb44-790a83f99a24-N-Salvia-clevelandii-8-23.JPG"
+                    width={3939 * 0.1}
+                    height={3456 * 0.1}
+                    defaultGrey
+                    sizes="50vw"
+                    objectFit="contain"
+                  />
+                </MediaDialogOnClick>
+              </ChildBox>
+            </RowBox>
+            <Spacing size="x-large" />
+            <Box display="flex">
+              <MediaDialogOnClick
+                mediaUrl="https://imgix.cosmicjs.com/296878b0-f4d4-11ed-bb44-790a83f99a24-N-Salvia-clevelandii-5-16-3.JPG"
+                mediaName="'Winnifred Gilman' Cleveland Sage, Salvia clevelandii 'Winnifred Gilman'"
+                mediaPreviewDialogProps={{
+                  width: 5167 * 0.15,
+                  height: 3445 * 0.15
+                }}
+              >
+                <ImageFancier
+                  layout="intrinsic"
+                  alt="'Winnifred Gilman' Cleveland Sage, Salvia clevelandii 'Winnifred Gilman'"
+                  src="https://imgix.cosmicjs.com/296878b0-f4d4-11ed-bb44-790a83f99a24-N-Salvia-clevelandii-5-16-3.JPG"
+                  width={5167 * 0.15}
+                  height={3445 * 0.15}
+                  defaultGrey
+                  sizes="50vw"
+                  objectFit="contain"
+                />
+              </MediaDialogOnClick>
+            </Box>
+          </Box>
+          <Spacing size="x-large" />
+          <Type variant="h4">'Winnifred Gilman' Cleveland Sage</Type>
+          <Type variant="subtitle1" gutterBottom>
+            <em>Salvia clevelandii 'Winnifred Gilman'</em>
+          </Type>
+          <Type variant="subtitle1" gutterBottom>
+            <em>By Laurie Meyerpeter, Master Gardener</em>
+          </Type>
+          <Type paragraph>
+            Along with gorgeous flowers, the leaves of 'Winifred Gilman' and
+            other Cleveland sage cultivars are highly aromatic. When the Placer
+            County Master Gardeners teach a propagation class at Del Oro High
+            School, this plant is a huge favorite of the teens because of its
+            beautiful aroma and the unique flowers.
+          </Type>
+          <Type paragraph>
+            'Winifred Gilman' is one cultivar of Cleveland sage but local
+            nurseries may stock similar and interchangeable Cleveland sage
+            cultivars such as 'Allen Chickering'. And the terms “salvia” and
+            “sage” are both used interchangeable to describe this very large
+            family of plants. Many of them are California natives. Salvias are
+            beloved by hummingbirds, bees, and butterflies.
+          </Type>
+          <Type paragraph>
+            Bloom: 'Winifred Gilman' blooms in late spring into early summer,
+            with a light bloom later in the summer. The distinctive whorled
+            flowers are shades of blue and violet. When not in bloom, the plant
+            forms a mound of gray-green leaves that are very aromatic on warm
+            summer days.
+          </Type>
+          <Type paragraph>Water needs: Low to Very Low once established.</Type>
+          <Type paragraph>Sun: Full sun, part shade.</Type>
+          <Type paragraph>
+            Size: 'Winifred Gilman' Cleveland sage grows 3-4' x 3-4' mounded
+            shrub if unpruned. Other cultivars may be slightly larger and wider
+            if unpruned.
+          </Type>
+          <Type paragraph>
+            Care: Low water requirements. Lightly prune in late fall to maintain
+            compact form.
+          </Type>
+          <Type paragraph>
+            Availability: Cleveland sage is seasonally available. Local
+            nurseries typically stock either 'Winifred Gilman' Cleveland Sage or
+            'Allan Chickering' Cleveland Sage throughout the spring and summer
+            months.
+          </Type>
+          <Type paragraph>
+            Bonus plants: Many other salvias or sages are Summer Strong plants
+            with low water requirements and good summer looks. Besides
+            'Winnifred Gilman', 'Allen Chickering', and other Cleveland sages
+            and cultivars, check out these readily available salvias: 'Bee's
+            Bliss' Sage (Salvia 'Bees Bliss'), White Sage (Salvia apiana), and
+            Hummingbird Sage (Salvia spathacea). A fantastic and popular family
+            of plants! Available seasonally at local nurseries.
+          </Type>
+          <Spacing />
+          <Type variant="subtitle1" gutterBottom>
+            Information:
+          </Type>
+          <Type>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="body1"
+              href="https://ucanr.edu/sites/WUCOLS/files/183488.pdf"
+            >
+              WUCOLS (https://ucanr.edu)
+            </Link>
+          </Type>
+          <Type>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="body1"
+              href="https://arboretum.ucdavis.edu/plant/Winnifred-Gilman-Cleveland-sage"
+            >
+              UC Davis Arboretum All Stars: (https://arboretum.ucdavis.edu)
+            </Link>
+          </Type>
+          <Type>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="body1"
+              href="https://calscape.org/Salvia-clevelandii-'Winnifred-Gilman'-(Winnifred-Gilman-Cleveland-Sage)?srchcr=sc646565c122cdb"
+            >
+              CalScape (https://calscape.org)
+            </Link>
+          </Type>
+        </WideContainer>
+      </MainBox>
+    </PageLayout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const placeholders = await getImgixBlurHashes(imgixImages)
+
+    return {
+      props: {placeholders}
+    }
+  } catch (error) {
+    console.log(
+      'There was an error fetching placeholders for Summer Strong page',
+      error
+    )
+    return {props: {}}
+  }
+}
