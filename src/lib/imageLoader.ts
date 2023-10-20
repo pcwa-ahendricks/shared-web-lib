18ab7b373b67b7ba820168f01d6ae4b9bf1f5dec
Abe Hendricks <ahendricks@pcwa.net>
2023-10-20 11:58:42 -0700

    Removed various un-necessary dependencies.

--- src/lib/imageLoader.ts
+++ src/lib/imageLoader.ts
@@ -1,6 +1,4 @@
-import {ImageLoader} from 'next/legacy/image'
-import {stringify} from 'querystringify'
-import parse from 'url-parse'
+import {ImageLoader} from 'next/image'
 
 const imgixLoader: ImageLoader = ({src, width, quality}) => {
   return `https://imgix.cosmicjs.com/${src}?auto=format&w=${width}&q=${
@@ -9,13 +7,18 @@ const imgixLoader: ImageLoader = ({src, width, quality}) => {
 }
 
 const imgixUrlLoader: ImageLoader = ({src, width, quality}) => {
-  const parsed = parse(src, true)
-  const {query, origin, pathname} = parsed
+  const srcUrl = new URL(src)
+  const {origin, pathname, searchParams} = srcUrl
   // Ditch width, quality, and auto props if they were supplied in URL
-  const {w: _width, q: _quality, auto: _auto, ...rest} = query
-  const p = stringify(rest, false)
-  const qs = p ? `&${p}` : ''
-  return `${origin}${pathname}?auto=format&w=${width}&q=${quality || 75}${qs}`
+  searchParams.delete('w')
+  searchParams.delete('q')
+  searchParams.delete('auto')
+  searchParams.append('auto', 'format')
+  searchParams.append('w', width.toString())
+  searchParams.append('q', (quality || 75).toString())
+
+  const url = `${origin}${pathname}?${searchParams.toString()}`
+  return url
 }
 
 export default imgixLoader
