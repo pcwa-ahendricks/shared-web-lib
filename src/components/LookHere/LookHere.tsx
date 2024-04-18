c16f7d7b4b9470d7d6fae6e6a2a13803d0cfea6d
Abe Hendricks <ahendricks@pcwa.net>
2024-04-18 14:43:22 -0700

    <LookHere/> updated.

--- src/components/LookHere/LookHere.tsx
+++ src/components/LookHere/LookHere.tsx
@@ -6,21 +6,24 @@ import {
   useMediaQuery,
   useTheme
 } from '@mui/material'
-import JackinBox, {JackinBoxProps} from '@components/mui-jackinbox/JackinBox'
 import HandIcon from 'mdi-material-ui/HandPointingRight'
 import {useTimeoutFn} from 'react-use'
+import FadeOut from '@components/boxes/animate/FadeOut'
+import {AnimateProps} from '@components/boxes/animate/Animate'
+import SlideInEllipticFwdLeft from '@components/boxes/animate/SlideInEllipticLeftFwd'
 
 export default function LookHere({
   children,
   animate,
   ...rest
-}: BoxProps & Partial<JackinBoxProps>) {
+}: BoxProps & Partial<AnimateProps>) {
   const theme = useTheme()
   const isXs = useMediaQuery(theme.breakpoints.only('xs'))
   const [show, setShow] = useState(false)
   const [timeUp, setTimeUp] = useState(false)
 
-  useTimeoutFn(() => setTimeUp(true), 3000)
+  useTimeoutFn(() => setTimeUp(true), 2500)
+
   useEffect(() => {
     setShow(Boolean(!isXs && animate && timeUp))
   }, [isXs, animate, timeUp])
@@ -28,18 +31,19 @@ export default function LookHere({
   return (
     <Box position="relative" {...rest}>
       {children}
-      <JackinBox name="fadeOut" delay={5} animate={show} hideUntilAnimate>
-        <JackinBox
-          name="backInLeft"
-          position="absolute"
-          left={-180}
-          top={20}
-          zIndex={1}
-          width={140}
-          textAlign="center"
-          animate={show}
-          hideUntilAnimate
-        >
+      <SlideInEllipticFwdLeft
+        animate={show}
+        hiddenUntilAnimate
+        sx={{
+          position: 'absolute',
+          left: -180,
+          top: 20,
+          zIndex: 1,
+          width: 140,
+          textAlign: 'center'
+        }}
+      >
+        <FadeOut delay={5000} animate={show}>
           <Type
             component="em"
             variant="h4"
@@ -50,8 +54,8 @@ export default function LookHere({
           </Type>
           <br />
           <HandIcon color="primary" fontSize="large" />
-        </JackinBox>
-      </JackinBox>
+        </FadeOut>
+      </SlideInEllipticFwdLeft>
     </Box>
   )
 }
---
+++ src/components/boxes/animate/SlideInFwdLeft.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Animate, {AnimateProps} from './Animate'
+
+type SlideInFwdLeftProps = Omit<AnimateProps, 'name' | 'easingFunc'>
+
+const SlideInFwdLeft = ({children, ...rest}: SlideInFwdLeftProps) => {
+  return (
+    <Animate
+      name="slide-in-fwd-left"
+      easingFunc="cubic-bezier(0.250, 0.460, 0.450, 0.940)"
+      duration={400}
+      animate3d
+      {...rest}
+    >
+      {children}
+    </Animate>
+  )
+}
+
+export default SlideInFwdLeft
