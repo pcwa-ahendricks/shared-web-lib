0409c6c8e559a6a8ed2ef9876589c602aa1acfa9
Abe Hendricks <ahendricks@pcwa.net>
2024-04-18 07:29:00 -0700

    Added <Animate/> props.

--- src/components/boxes/animate/Animate.tsx
+++ src/components/boxes/animate/Animate.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import {Box} from '@mui/material'
 import {BoxProps} from '@mui/material/Box'
-export type AnimateProps = {animate?: boolean; delay?: number} & BoxProps
+export type AnimateProps = {
+  animate?: boolean
+  duration?: number
+  delay?: number
+  iterations?: number
+  direction?: 'normal' | 'reverse' | 'alternate' | 'alternate-reverse'
+  fillMode?: 'both' | 'backwards' | 'forwards' | 'none'
+} & BoxProps
 
 const Animate = ({children, delay, animate, ...rest}: AnimateProps) => {
   return <Box {...rest}>{children}</Box>
--- src/components/boxes/animate/SlideInLeft.tsx
+++ src/components/boxes/animate/SlideInLeft.tsx
@@ -4,7 +4,11 @@ import Animate, {AnimateProps} from './Animate'
 const SlideInLeft = ({
   children,
   delay = 0,
+  duration = 500,
   animate = true,
+  iterations = 1,
+  direction = 'normal',
+  fillMode = 'both',
   ...rest
 }: AnimateProps) => {
   const {sx, ...restBoxProps} = rest
@@ -12,8 +16,8 @@ const SlideInLeft = ({
     <Animate
       sx={{
         ...(animate && {
-          // '-webkit-animation': `slide-in-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms both`,
-          animation: `slide-in-left 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms both`
+          // '-webkit-animation': `slide-in-left ${duration}ms cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms ${iterations} ${direction} ${fillMode}`,
+          animation: `slide-in-left ${duration}ms cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms ${iterations} ${direction} ${fillMode}`
         }),
         ...sx
       }}
