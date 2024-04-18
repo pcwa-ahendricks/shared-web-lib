c7bf405c3f8f09c512be9a0702e1d1552c119a82
Abe Hendricks <ahendricks@pcwa.net>
2024-04-18 07:38:31 -0700

    Put <SlideInLeft/> props in <Animate/> base component.

--- src/components/boxes/animate/Animate.tsx
+++ src/components/boxes/animate/Animate.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Box} from '@mui/material'
 import {BoxProps} from '@mui/material/Box'
 export type AnimateProps = {
+  name: string
   animate?: boolean
   duration?: number
   delay?: number
@@ -10,8 +11,32 @@ export type AnimateProps = {
   fillMode?: 'both' | 'backwards' | 'forwards' | 'none'
 } & BoxProps
 
-const Animate = ({children, delay, animate, ...rest}: AnimateProps) => {
-  return <Box {...rest}>{children}</Box>
+const Animate = ({
+  children,
+  delay = 0,
+  duration = 500,
+  animate = true,
+  iterations = 1,
+  direction = 'normal',
+  fillMode = 'both',
+  name,
+  ...rest
+}: AnimateProps) => {
+  const {sx, ...restBoxProps} = rest
+  return (
+    <Box
+      sx={{
+        ...(animate && {
+          // '-webkit-animation': `${name} ${duration}ms cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms ${iterations} ${direction} ${fillMode}`,
+          animation: `${name} ${duration}ms cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms ${iterations} ${direction} ${fillMode}`
+        }),
+        ...sx
+      }}
+      {...restBoxProps}
+    >
+      {children}
+    </Box>
+  )
 }
 
 export default Animate
--- src/components/boxes/animate/SlideInLeft.tsx
+++ src/components/boxes/animate/SlideInLeft.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
 import Animate, {AnimateProps} from './Animate'
 
-const SlideInLeft = ({
-  children,
-  delay = 0,
-  duration = 500,
-  animate = true,
-  iterations = 1,
-  direction = 'normal',
-  fillMode = 'both',
-  ...rest
-}: AnimateProps) => {
-  const {sx, ...restBoxProps} = rest
+type Props = Omit<AnimateProps, 'name'>
+
+const SlideInLeft = ({children, ...rest}: Props) => {
   return (
-    <Animate
-      sx={{
-        ...(animate && {
-          // '-webkit-animation': `slide-in-left ${duration}ms cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms ${iterations} ${direction} ${fillMode}`,
-          animation: `slide-in-left ${duration}ms cubic-bezier(0.25, 0.46, 0.45, 0.94) ${delay}ms ${iterations} ${direction} ${fillMode}`
-        }),
-        ...sx
-      }}
-      {...restBoxProps}
-    >
+    <Animate name="slide-in-left" {...rest}>
       {children}
     </Animate>
   )
