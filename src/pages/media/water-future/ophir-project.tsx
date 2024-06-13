cfcc915b617019e48e01625ed6baf5ebdcbcddab
Abe Hendricks <ahendricks@pcwa.net>
2024-06-13 12:13:57 -0700

    Images added to Ophir Project page.

--- src/pages/media/water-future/ophir-project.tsx
+++ src/pages/media/water-future/ophir-project.tsx
@@ -4,8 +4,28 @@ import MainBox from '@components/boxes/MainBox'
 import NarrowContainer from '@components/containers/NarrowContainer'
 import Image from 'next/image'
 import {Box} from '@mui/material'
+import {GetStaticProps} from 'next'
+import {getImgixBlurHashes} from '@components/imageBlur/ImageBlur'
+import usePlaceholders from '@components/imageBlur/usePlaceholders'
+import {Placeholders} from '@components/imageBlur/ImageBlurStore'
+import Spacing from '@components/boxes/Spacing'
 
-export default function OphirProjectPage() {
+const imgixImages = [
+  'https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/Cover.png',
+  'https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/About.png',
+  'https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/Building.png',
+  "https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/Funding%20Tomorrow's%20Water.png",
+  'https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/Empowering.png',
+  'https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/The%202030%20Vision.png'
+]
+
+type Props = {
+  placeholders: Placeholders
+}
+
+export default function OphirProjectPage({placeholders}: Props) {
+  usePlaceholders(placeholders)
+
   return (
     <PageLayout title="Ophir Project" waterSurface>
       <MainBox>
@@ -32,11 +52,11 @@ export default function OphirProjectPage() {
                 pointerEvents: 'none'
               }}
             />
+            <Spacing />
             <Image
-              priority
               alt="Building page on Ophir Project Brochure"
-              width={1112}
-              height={2186}
+              width={1113}
+              height={2500}
               src="https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/Building.png"
               sizes={`(max-width: 700px) 100vw, 700px`}
               draggable="false"
@@ -48,11 +68,59 @@ export default function OphirProjectPage() {
                 pointerEvents: 'none'
               }}
             />
+            <Spacing />
             <Image
-              priority
+              alt="Empowering page on Ophir Project Brochure"
+              width={1112}
+              height={2399}
+              src="https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/Empowering.png"
+              sizes={`(max-width: 700px) 100vw, 700px`}
+              draggable="false"
+              style={{
+                width: '100%',
+                height: 'auto',
+                objectFit: 'contain',
+                userSelect: 'none',
+                pointerEvents: 'none'
+              }}
+            />
+            <Spacing />
+            <Image
+              alt="Funding Tomorrow's Future page on Ophir Project Brochure"
+              width={1075}
+              height={2500}
+              src="https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/Funding%20Tomorrow's%20Water.png"
+              sizes={`(max-width: 700px) 100vw, 700px`}
+              draggable="false"
+              style={{
+                width: '100%',
+                height: 'auto',
+                objectFit: 'contain',
+                userSelect: 'none',
+                pointerEvents: 'none'
+              }}
+            />
+            <Spacing size="x-large" />
+            <Image
+              alt="The 2030 Vision page on Ophir Project Brochure"
+              width={1075}
+              height={2550}
+              src="https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/The%202030%20Vision.png"
+              sizes={`(max-width: 700px) 100vw, 700px`}
+              draggable="false"
+              style={{
+                width: '100%',
+                height: 'auto',
+                objectFit: 'contain',
+                userSelect: 'none',
+                pointerEvents: 'none'
+              }}
+            />
+            <Spacing size="x-large" />
+            <Image
               alt="About page on Ophir Project Brochure"
               width={1112}
-              height={2186}
+              height={1232}
               src="https://pcwa.imgix.net/pcwa-net/media/water-future/ophir-project/About.png"
               sizes={`(max-width: 700px) 100vw, 700px`}
               draggable="false"
@@ -69,4 +137,17 @@ export default function OphirProjectPage() {
       </MainBox>
     </PageLayout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const placeholders = await getImgixBlurHashes(imgixImages)
+
+    return {
+      props: {placeholders}
+    }
+  } catch (error) {
+    console.log('There was an error getting Imgix blur hashes', error)
+    return {props: {}}
+  }
 }
