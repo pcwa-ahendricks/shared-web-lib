829b049da11b42b7cf0506123e194d5e0608889e
Abe Hendricks <ahendricks@pcwa.net>
2024-08-27 09:15:36 -0700

    Added MediaDialogOnClick and MediaPreviewDialog components.

---
+++ mui_next/components/MediaDialogOnClick.tsx
@@ -0,0 +1,176 @@
+'use client'
+import React, {useState, useRef} from 'react'
+import {
+  Box,
+  Fade,
+  Popper,
+  Typography as Type,
+  useMediaQuery,
+  useTheme,
+  Theme
+} from '@mui/material'
+import MediaPreviewDialog, {
+  type MediaPreviewDialogProps
+} from './MediaPreviewDialog'
+import {styled} from '@mui/system'
+import colorAlpha from 'color-alpha'
+
+type Props = {
+  mediaName: string
+  mediaUrl: string
+  children: React.ReactNode
+  timeout?: number
+  popperMessage?: string
+  popperAnchorStyle?: React.CSSProperties
+  MediaPreviewDialogProps?: Partial<MediaPreviewDialogProps>
+  mediaDialogOpen?: boolean
+  showPopper?: boolean
+}
+
+const PREFIX = 'StyledMediaDialogOnClick'
+const classes = {
+  popper: `${PREFIX}-popper`,
+  childrenRoot: `${PREFIX}-childrenRoot`
+}
+const Root = styled(Box)(({theme}) => ({
+  [`& .${classes.popper}`]: {
+    position: 'absolute',
+    zIndex: 5,
+    pointerEvents: 'none'
+  },
+  [`& .${classes.childrenRoot}`]: {
+    [theme.breakpoints.up('sm')]: {
+      cursor: 'pointer'
+    }
+  }
+}))
+
+/**
+ * MediaDialogOnClick component provides a clickable UI element that opens a media preview dialog
+ * when clicked. It also optionally displays a popper message on hover to indicate that the image can be clicked to view a larger version.
+ *
+ * @param {Props} props - The properties passed to the component.
+ * @param {string} props.mediaName - The name of the media to be displayed in the dialog.
+ * @param {string} props.mediaUrl - The URL of the media to be displayed in the dialog.
+ * @param {React.ReactNode} props.children - The content to be rendered inside the clickable area.
+ * @param {number} [props.timeout=350] - The duration of the fade transition for the popper in milliseconds.
+ * @param {string} [props.popperMessage='Click to view larger image'] - The message displayed in the popper on hover.
+ * @param {React.CSSProperties} [props.popperAnchorStyle] - Optional styles for the popper anchor element.
+ * @param {Partial<MediaPreviewDialogProps>} [props.MediaPreviewDialogProps] - Additional props passed to the MediaPreviewDialog component.
+ * @param {boolean} [props.mediaDialogOpen=false] - Whether the media dialog is initially open.
+ * @param {boolean} [props.showPopper=true] - Whether to show the popper on hover.
+ * @returns {JSX.Element} The MediaDialogOnClick component.
+ */
+const MediaDialogOnClick = ({
+  children,
+  popperMessage = 'Click to view larger image',
+  popperAnchorStyle = {},
+  mediaName,
+  mediaUrl,
+  timeout = 350,
+  MediaPreviewDialogProps,
+  mediaDialogOpen: mediaDialogOpenProp = false,
+  showPopper = true,
+  ...rest
+}: Props) => {
+  const theme = useTheme<Theme>()
+  const isXs = useMediaQuery(theme.breakpoints.only('xs'))
+
+  // Div element helps with Popper positioning.
+  const popperAnchorEl = useRef<HTMLDivElement>(null)
+
+  const [mediaDialogOpen, setMediaDialogOpen] =
+    useState<boolean>(mediaDialogOpenProp)
+
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const handlePopoverOpen = (event: any) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null)
+  }
+
+  const imageClickHandler = () => {
+    if (!isXs) {
+      setMediaDialogOpen(true)
+    }
+  }
+
+  const open = Boolean(anchorEl)
+  const {
+    children: mediaPreviewDialogPropsChildren,
+    ...mediaPreviewDialogPropsRest
+  } = MediaPreviewDialogProps || {}
+
+  return (
+    <Root {...rest}>
+      <Box
+        aria-owns={open ? 'mouse-over-popover' : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+        onClick={imageClickHandler}
+        position="relative"
+      >
+        <div
+          ref={popperAnchorEl}
+          style={{
+            position: 'absolute',
+            top: '8px',
+            right: '8px',
+            ...popperAnchorStyle
+          }}
+        />
+        {/* Css implementation won't work here (eg. Popper will still show on xs devices). */}
+        {isXs || !showPopper ? null : (
+          <Popper
+            id="mouse-over-popover"
+            className={classes.popper}
+            open={open}
+            anchorEl={popperAnchorEl.current}
+            transition
+            placement="bottom-end"
+          >
+            {({TransitionProps}) => (
+              <Fade {...TransitionProps} timeout={timeout}>
+                <Box
+                  sx={{
+                    borderRadius: 1,
+                    border: 1,
+                    borderColor: 'grey.300', // Must be specified after border prop. Might not be necessary in future versions of @mui/material.
+                    p: 1,
+                    m: 1,
+                    fontStyle: 'italic',
+                    bgcolor: colorAlpha(theme.palette.background.paper, 1),
+                    cursor: 'pointer'
+                  }}
+                >
+                  <Type variant="body2">{popperMessage}</Type>
+                </Box>
+              </Fade>
+            )}
+          </Popper>
+        )}
+        <Box className={classes.childrenRoot}>{children}</Box>
+      </Box>
+      <MediaPreviewDialog
+        open={mediaDialogOpen}
+        onClose={() => setMediaDialogOpen(false)}
+        name={mediaName}
+        url={mediaUrl}
+        scroll="body"
+        fullWidth={false}
+        maxWidth="xl"
+        {...mediaPreviewDialogPropsRest}
+        // showActions
+        // dlUrl={`${ImageUrl}${qsDownloadUrl}`}
+      >
+        {mediaPreviewDialogPropsChildren}
+      </MediaPreviewDialog>
+    </Root>
+  )
+}
+
+export default MediaDialogOnClick
---
+++ mui_next/components/MediaPreviewDialog.tsx
@@ -0,0 +1,192 @@
+'use client'
+
+import React, {useMemo, useCallback} from 'react'
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  Fab,
+  DialogProps,
+  Zoom,
+  Box,
+  BoxProps,
+  useTheme
+} from '@mui/material'
+import DeleteIcon from '@mui/icons-material/CloseRounded'
+import Image, {type ImageProps} from 'next/image'
+import {useWindowSize} from 'react-use'
+import imgixUrlLoader from '../../next/imgixUrlLoader'
+
+export type MediaPreviewDialogProps = {
+  name: string
+  open: boolean
+  onClose: () => void
+  showActions?: boolean
+  url: string | string[]
+  dlUrl?: string
+  width?: ImageProps['width']
+  height?: ImageProps['height']
+  native?: boolean
+  dialogWidth?: BoxProps['width']
+  factorWidth?: number
+} & Partial<DialogProps>
+
+/**
+ * MediaPreviewDialog component provides a modal dialog to preview media content such as images.
+ * The component supports multiple display options, including full width, portrait, and landscape orientations.
+ * It also offers actions like downloading the media or closing the dialog. Specify Image width
+ * and height OR use fullWidth property, which works well with portrait orientation images, though,
+ * with landscape images it will likely result with top & bottom whitespace.
+ *
+ * @param {MediaPreviewDialogProps} props - The properties passed to the component.
+ * @param {string} props.name - The name of the media to be displayed, used as the alt text for the image.
+ * @param {boolean} props.open - Controls whether the dialog is open or closed.
+ * @param {() => void} props.onClose - Callback function to handle closing the dialog.
+ * @param {boolean} [props.showActions=false] - If true, displays actions such as download and close buttons.
+ * @param {string | string[]} props.url - The URL or an array of URLs of the media to be displayed.
+ * @param {string} [props.dlUrl] - The URL to download a copy of the media.
+ * @param {ImageProps['width']} [props.width] - The width of the media (optional).
+ * @param {ImageProps['height']} [props.height] - The height of the media (optional).
+ * @param {boolean} [props.native=false] - If true, uses a native <img> tag instead of the Next.js Image component.
+ * @param {BoxProps['width']} [props.dialogWidth] - Custom width for the dialog box.
+ * @param {number} [props.factorWidth] - Factor width for responsive image sizing (between 0-1).
+ * @returns {JSX.Element} The MediaPreviewDialog component.
+ */
+const MediaPreviewDialog = ({
+  onClose: onCloseProp,
+  name,
+  width,
+  height,
+  url,
+  dlUrl,
+  dialogWidth,
+  factorWidth, // Between 0-1
+  showActions = false,
+  open = false,
+  native = false,
+  children,
+  ...rest
+}: MediaPreviewDialogProps) => {
+  const theme = useTheme()
+  const {width: ww, height: wh} = useWindowSize()
+  const factor = factorWidth ? factorWidth * 100 : 86
+  const isPortraitWindow = ww <= wh
+  const imageAspectRatio =
+    typeof width === 'number' && typeof height === 'number'
+      ? width / height
+      : null
+  const factoredVmin = `${(imageAspectRatio ?? 1) * factor}vmin`
+  const factoredVmax = `${(imageAspectRatio ?? 1) * factor}vmax`
+  dialogWidth = (dialogWidth ?? isPortraitWindow) ? factoredVmax : factoredVmin
+
+  const getImgEl = useCallback(
+    (url: string, key?: string | number) =>
+      !native ? (
+        <Image
+          key={key}
+          loader={imgixUrlLoader}
+          src={url}
+          alt={name}
+          width={width}
+          height={height}
+          style={{
+            width: '100%',
+            height: 'auto',
+            objectFit: 'contain'
+          }}
+        />
+      ) : (
+        // eslint-disable-next-line  @next/next/no-img-element
+        <img
+          key={key}
+          style={{display: 'block'}} // remove bottom margin resulting from "inline-block"
+          object-fit="contain"
+          // data-sizes="auto"
+          // data-src={url}
+          src={url} // IE fix - src attribute may be required for displaying img.
+          alt={name}
+        />
+      ),
+    [name, width, height, native]
+  )
+
+  const imgEl = useMemo(
+    () =>
+      Array.isArray(url)
+        ? url.map((urlItem, idx) => getImgEl(urlItem, idx))
+        : getImgEl(url),
+    [url, getImgEl]
+  )
+
+  const CondDialogActions = useCallback(
+    () =>
+      showActions ? (
+        <DialogActions>
+          {dlUrl ? (
+            <Button href={dlUrl} target="_blank" rel="noopener">
+              Download Copy
+            </Button>
+          ) : null}
+          <Button color="primary" onClick={onCloseProp}>
+            Close
+          </Button>
+        </DialogActions>
+      ) : null,
+    [showActions, dlUrl, onCloseProp]
+  )
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onCloseProp}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      TransitionComponent={Zoom}
+      PaperProps={{
+        square: true,
+        sx: {
+          overflow: 'visible',
+          overflowY: 'visible',
+          overflowX: 'visible'
+        }
+      }}
+      {...rest}
+    >
+      <>
+        <Fab
+          size="small"
+          onClick={onCloseProp}
+          aria-label="Close Dialog"
+          sx={{
+            zIndex: 2,
+            position: 'absolute',
+            transform: 'translate(-50%, -50%)'
+          }}
+        >
+          <DeleteIcon />
+        </Fab>
+        <DialogContent
+          sx={{
+            '.root': {
+              margin: 0,
+              padding: 0,
+              overflowX: 'hidden'
+            }
+          }}
+        >
+          <Box width={dialogWidth} maxWidth="100%">
+            <Box sx={{backgroundColor: theme.palette.common.white}}>
+              {imgEl}
+            </Box>
+            {children ? <Box>{children}</Box> : null}
+          </Box>
+        </DialogContent>
+        <CondDialogActions />
+      </>
+    </Dialog>
+  )
+}
+
+// withMobileDialog()((MediaPreviewDialog))
+export default MediaPreviewDialog
