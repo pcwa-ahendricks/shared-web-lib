7e492def4e24456351d0f6c164e41dd07c6eb3ec
Abe Hendricks <ahendricks@pcwa.net>
2026-01-30 11:59:57 -0800

    Added slide in left animista animation

--- tw/animista.mjs
+++ tw/animista.mjs
@@ -4,7 +4,9 @@ const animista = {
       'mista-fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both', // https://animista.net/play/entrances/fade-in
     'mista-fade-out': 'mista-fade-out 1s ease-out both', // https://animista.net/play/exits/fade-out
     'mista-scale-in-center':
-      'mista-scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both' // https://animista.net/play/entrances/scale-in/scale-in-center
+      'mista-scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both', // https://animista.net/play/entrances/scale-in/scale-in-center
+    'mista-slide-in-left':
+      'mista-slide-in-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both' // https://animista.net/play/entrances/slide-in/slide-in-left
   },
   keyframes: {
     // https://animista.net/play/entrances/fade-in
@@ -25,6 +27,17 @@ const animista = {
       },
       '100%': {
         transform: 'scale(1)',
+        opacity: '1'
+      }
+    },
+    // https://animista.net/play/entrances/slide-in/slide-in-left
+    'mista-slide-in-left': {
+      '0%': {
+        transform: 'translateX(-1000px)',
+        opacity: '0'
+      },
+      '100%': {
+        transform: 'translateX(0)',
         opacity: '1'
       }
     }
