f3cf83052b10e742cddb821f6154815019048935
Abe Hendricks <ahendricks@pcwa.net>
2026-02-11 12:25:50 -0800

    Update types used in useLinkComponent

--- next/hooks/useLinkComponent.tsx
+++ next/hooks/useLinkComponent.tsx
@@ -9,7 +9,7 @@ import {forwardRef, useMemo} from 'react'
  * The resulting `LinkComponent` is memoized to avoid unnecessary re-renders.
  *
  * @param {Partial<LinkProps>} [props] - Optional custom props to pass to the `Link` component.
- * @returns {React.ComponentType<LinkProps>} The memoized `LinkComponent` with the specified props.
+ * @returns {React.ComponentType<Omit<LinkProps, 'ref'>>} The memoized `LinkComponent` with the specified props.
  *
  * @example
  * const LinkComponent = useLinkComponent({ noLinkStyle: false });
@@ -18,24 +18,25 @@ import {forwardRef, useMemo} from 'react'
  *   <Button LinkComponent={LinkComponent} href="/some/route/or/ext/link">
  * );
  */
-export default function useLinkComponent(props?: Partial<LinkProps>) {
+export default function useLinkComponent(
+  props?: Partial<Omit<LinkProps, 'ref'>>
+) {
   // default noLinkStyle prop to true
   const {noLinkStyle = true, ...rest} = props || {}
   const LinkComponent = useMemo(
     () =>
-      forwardRef<HTMLAnchorElement, LinkProps>(function LinkComponent(
-        forwardRefProps: LinkProps,
-        ref
-      ) {
-        return (
-          <Link
-            noLinkStyle={noLinkStyle}
-            ref={ref}
-            {...rest}
-            {...forwardRefProps}
-          />
-        )
-      }),
+      forwardRef<HTMLAnchorElement, Omit<LinkProps, 'ref'>>(
+        function LinkComponent(forwardRefProps: Omit<LinkProps, 'ref'>, ref) {
+          return (
+            <Link
+              noLinkStyle={noLinkStyle}
+              ref={ref}
+              {...rest}
+              {...forwardRefProps}
+            />
+          )
+        }
+      ),
     [rest, noLinkStyle]
   )
 
