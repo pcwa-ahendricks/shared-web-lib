877787084cdeea8299a7d597c5b8654f12cd245d
Abe Hendricks <ahendricks@pcwa.net>
2026-04-07 16:03:29 -0700

    Don't use opacity by default with Link

--- tw/Link.tsx
+++ tw/Link.tsx
@@ -51,9 +51,9 @@ const Link = forwardRef<HTMLAnchorElement, LinkProps>(
       <Comp
         ref={ref}
         className={cn(
-          'decoration-current decoration-1 underline-offset-4 opacity-90 ' +
-            'transition-colors hover:decoration-[1.5px] hover:opacity-100 ' +
-            'focus-visible:underline focus-visible:decoration-[1.5px] focus-visible:opacity-100',
+          'decoration-current decoration-1 underline-offset-4',
+          'transition-colors hover:decoration-[1.5px]',
+          'focus-visible:underline focus-visible:decoration-[1.5px]',
           underline === 'always' && 'underline',
           // tighten hover underline styles for better UX
           underline === 'hover' &&
